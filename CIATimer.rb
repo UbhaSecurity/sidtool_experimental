8bf956459f454bcd64081af28f3777326fbdbf96
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-11 22:49:29 +0100

    Update CIATimer.rb
    Updated integration

--- CIATimer.rb
+++ CIATimer.rb
@@ -1,15 +1,18 @@
 module Sidtool
   class CIATimer
+    # Constants for control register flags
     START_FLAG = 0x01       # Timer start/stop
     ONESHOT_FLAG = 0x08     # One-shot mode
     PHASE_FLAG = 0x10       # Timer counts system clocks (PHI2) or underflows from other timer
     TOGGLE_FLAG = 0x40      # Timer output toggles between high and low on underflow
     INTERRUPT_FLAG = 0x80   # Enable interrupt on timer underflow
 
-    attr_accessor :timer, :latch, :control_register, :underflow
+    attr_accessor :timer, :latch, :control_register, :underflow, :output_pin
 
-    def initialize
+    def initialize(cpu)
       reset
+      @cpu = cpu  # CPU instance for triggering interrupts
+      @output_pin = false  # State of the timer's output pin
     end
 
     # Reset the timer to its default state
@@ -18,6 +21,7 @@ module Sidtool
       @latch = 0xFFFF
       @control_register = 0
       @underflow = false
+      @output_pin = false
     end
 
     # Update the timer, typically called each system clock cycle
@@ -26,6 +30,22 @@ module Sidtool
         decrement_timer
         check_underflow
       end
+    end
+
+    # Decrement the timer
+    def decrement_timer
+      if (@control_register & PHASE_FLAG) != 0
+        @timer -= 1 if @timer > 0
+      else
+        decrement_on_external_event if external_event_occurred?
+      end
+    end
+
+    # Handle the timer underflow, such as triggering an interrupt
+    def handle_underflow
+      @underflow = true
+      toggle_output if (@control_register & TOGGLE_FLAG) != 0
+      trigger_interrupt if (@control_register & INTERRUPT_FLAG) != 0
     end
 
     # Check for timer underflow and handle accordingly
@@ -41,27 +61,14 @@ module Sidtool
       @timer = @latch
     end
 
-    def decrement_timer
-      if (@control_register & PHASE_FLAG) != 0
-        @timer -= 1 if @timer > 0
-      else
-        # Logic to decrement based on external events or other timer's underflow
-      end
-    end
-
-    # Handle the timer underflow, such as triggering an interrupt
-    def handle_underflow
-      @underflow = true
-      toggle_output if (@control_register & TOGGLE_FLAG) != 0
-      trigger_interrupt if (@control_register & INTERRUPT_FLAG) != 0
-    end
-
+    # Toggle the output pin state
     def toggle_output
-      # Logic to toggle timer output
+      @output_pin = !@output_pin
     end
 
+    # Trigger an interrupt via the CPU
     def trigger_interrupt
-      # Logic to trigger an interrupt
+      @cpu.trigger_interrupt(:cia_timer_underflow)
     end
 
     # Determine if the timer is running
@@ -89,7 +96,16 @@ module Sidtool
       @control_register = value
     end
 
-    # Additional methods as needed for timer functionality
-    # ...
+    private
+
+    # Logic to decrement the timer based on external events
+    def decrement_on_external_event
+      # Implement external event logic
+    end
+
+    # Logic to determine if an external event occurred
+    def external_event_occurred?
+      # Implement external event check
+    end
   end
 end
