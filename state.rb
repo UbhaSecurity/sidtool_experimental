4331417801e77dde5532d6515670a4aa85b32f4f
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-11 23:28:42 +0100

    Update state.rb
    Integrated

--- state.rb
+++ state.rb
@@ -6,14 +6,17 @@ module Sidtool
 
     def initialize
       @current_frame = 0
-      @sid6581 = Sid6581.new # Assuming Sid6581 is a class managing SID registers
-      @cia_timers = [CIATimer.new, CIATimer.new] # Assuming two CIA timers for simplicity
+      @sid6581 = Sid6581.new
+      @cia_timers = [CIATimer.new(self), CIATimer.new(self)]
     end
 
     # Update the state for the current frame
     def update
       # Update CIA timers
       @cia_timers.each(&:update)
+
+      # Check for and handle interrupts
+      handle_interrupts
 
       # Update SID chip state, e.g., process sound generation for the current frame
       @sid6581.update
@@ -22,7 +25,21 @@ module Sidtool
       @current_frame += 1
     end
 
+    private
+
+    # Handle interrupts from CIA timers or other sources
+    def handle_interrupts
+      @cia_timers.each do |timer|
+        if timer.underflow
+          # Handle the specific actions to be taken on timer underflow
+          # This could include triggering interrupts or other responses
+        end
+      end
+
+      # Additional logic for other interrupt sources or state changes
+    end
+
     # Additional methods related to state management can be added here
-    # For example, methods to handle interrupts, synchronization with other components, etc.
+    # For example, methods to synchronize with other components, etc.
   end
 end
