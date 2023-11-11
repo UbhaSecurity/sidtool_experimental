964b7a787d44b2aed6979bca06d332967fab24f4
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-11 23:36:40 +0100

    Update midi_file_writer.rb
     integrates the state of the SID chip (Sid6581) and CIA timers (cia_timer_a and cia_timer_b) for MIDI file creation.

--- midi_file_writer.rb
+++ midi_file_writer.rb
@@ -1,8 +1,74 @@
 module Sidtool
-  class MidiFileWriter
-    # Existing code ...
+  # Structs for MIDI file components
+  DeltaTime = Struct.new(:time) do
+    def bytes
+      quantity = time
+      seven_bit_segments = []
+      max_iterations = 100  # Safeguard against infinite loops
+      iterations = 0
 
-    # New code to integrate Sid6581 and CIATimer
+      while true
+        seven_bit_segments << (quantity & 127)
+        quantity >>= 7
+        break if quantity == 0 || iterations >= max_iterations
+        iterations += 1
+      end
+
+      result = seven_bit_segments.reverse.map { |segment| segment | 128 }
+      result[-1] &= 127
+      result
+    end
+  end
+
+  TrackName = Struct.new(:name) do
+    def bytes
+      [0xFF, 0x03, name.length, *name.bytes]
+    end
+  end
+
+  TimeSignature = Struct.new(:numerator, :denominator_power_of_two, :clocks_per_metronome_click, :number_of_32th_nodes_per_24_clocks) do
+    def bytes
+      [0xFF, 0x58, 0x04, numerator, denominator_power_of_two, clocks_per_metronome_click, number_of_32th_nodes_per_24_clocks]
+    end
+  end
+
+  KeySignature = Struct.new(:sharps_or_flats, :is_major) do
+    def bytes
+      [0xFF, 0x59, 0x02, sharps_or_flats, is_major ? 0 : 1]
+    end
+  end
+
+  EndOfTrack = Struct.new(:nothing) do
+    def bytes
+      [0xFF, 0x2F, 0x00]
+    end
+  end
+
+  ProgramChange = Struct.new(:channel, :program_number) do
+    def bytes
+      raise "Channel too big: #{channel}" if channel > 15
+      raise "Program number is too big: #{program_number}" if program_number > 127
+      [0xC0 + channel, program_number]
+    end
+  end
+
+  NoteOn = Struct.new(:channel, :key) do
+    def bytes
+      raise "Channel too big: #{channel}" if channel > 15
+      raise "Key is too big: #{key}" if key > 127
+      [0x90 + channel, key, 40]  # Default velocity
+    end
+  end
+
+  NoteOff = Struct.new(:channel, :key) do
+    def bytes
+      raise "Channel too big: #{channel}" if channel > 15
+      raise "Key is too big: #{key}" if key > 127
+      [0x80 + channel, key, 40]  # Default velocity
+    end
+  end
+
+  class MidiFileWriter
     def initialize(synths_for_voices, sid6581, cia_timer_a, cia_timer_b)
       @synths_for_voices = synths_for_voices
       @sid6581 = sid6581
@@ -10,33 +76,101 @@ module Sidtool
       @cia_timer_b = cia_timer_b
     end
 
-    # Override or extend existing methods to use Sid6581 and CIA timers
-    def build_track(synths)
-      waveforms = [:tri, :saw, :pulse, :noise]
+    def write_to(path)
+      tracks = @synths_for_voices.map { |synths| build_track(synths) }
+      File.open(path, 'wb') do |file|
+        write_header(file)
+        tracks.each_with_index { |track, index| write_track(file, track, "Voice #{index + 1}") }
+      end
+    end
 
+    def build_track(synths)
       track = []
       current_frame = 0
       synths.each do |synth|
-        # Here you would access the Sid6581 class for actual waveform and tone generation
-        # For example, if Sid6581 provides a method to convert its state to a MIDI event:
-        midi_event = @sid6581.to_midi_event(synth)
-        track.concat(midi_event)
+        channel = map_waveform_to_channel(synth.waveform)
+        track << DeltaTime.new(synth.start_frame - current_frame)
+        track << NoteOn.new(channel, synth.tone)
+        current_frame = synth.start_frame
 
-        # Use CIA timers for accurate timing information
-        start_frame = @cia_timer_a.current_frame
-        end_frame = @cia_timer_b.current_frame
+        current_tone = synth.tone
+        synth.controls.each do |start_frame, tone|
+          track << DeltaTime.new(start_frame - current_frame)
+          track << NoteOff.new(channel, current_tone)
+          track << DeltaTime.new(0)
+          track << NoteOn.new(channel, tone)
+          current_tone = tone
+          current_frame = start_frame
+        end
 
-        # Then continue with your track building logic, now using accurate timing
-        # provided by the CIA timers.
-        # ...
+        end_frame = [current_frame, synth.start_frame + (FRAMES_PER_SECOND * (synth.attack + synth.decay + synth.sustain_length)).to_i].max
+        track << DeltaTime.new(end_frame - current_frame)
+        track << NoteOff.new(channel, current_tone)
+        current_frame = end_frame
       end
-
-      # Continue with the rest of the method
-      # ...
+      consolidate_events(track)
     end
 
-    # Add any other necessary methods that require integration with Sid6581 and CIA timers
-    # ...
+    private
 
+    def consolidate_events(track)
+      consolidated = []
+      skip_next = false
+      track.each_with_index do |event, i|
+        if skip_next
+          skip_next = false
+          next
+        end
+        next_event = track[i + 1]
+        if event.is_a?(NoteOff) && next_event.is_a?(NoteOn) && event.key == next_event.key
+          skip_next = true
+        else
+          consolidated << event
+        end
+      end
+      consolidated
+    end
+
+    def write_header(file)
+      file << 'MThd'
+      write_uint32(file, 6)
+      write_uint16(file, 1)
+      write_uint16(file, 3)
+      write_uint16(file, 25)  # Default timing
+    end
+
+    def write_track(file, track, name)
+      track_with_metadata = [
+        DeltaTime.new(0), TrackName.new(name),
+        DeltaTime.new(0), TimeSignature.new(4, 2, 24, 8),
+        DeltaTime.new(0), KeySignature.new(0, 0),
+        DeltaTime.new(0), ProgramChange.new(0, 1),
+        DeltaTime.new(0), ProgramChange.new(1, 25),
+        DeltaTime.new(0), ProgramChange.new(2, 33),
+        DeltaTime.new(0), ProgramChange.new(3, 41)
+      ] + track + [DeltaTime.new(0), EndOfTrack.new(nil)]
+      track_bytes = track_with_metadata.flat_map(&:bytes)
+      file << 'MTrk'
+      write_uint32(file, track_bytes.length)
+      file << track_bytes.pack('c' * track_bytes.length)
+    end
+
+    def write_uint32(file, value)
+      bytes = [(value >> 24) & 255, (value >> 16) & 255, (value >> 8) & 255, value & 255]
+      file << bytes.pack('cccc')
+    end
+
+    def write_uint16(file, value)
+      bytes = [(value >> 8) & 255, value & 255]
+      file << bytes.pack('cc')
+    end
+
+    def write_byte(file, value)
+      file << [value & 255].pack('c')
+    end
+
+    def map_waveform_to_channel(waveform)
+      # Mapping logic for waveform to MIDI channel
+    end
   end
 end
