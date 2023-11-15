5172004090d3a221eefe0094424c028b1a08d28d
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-15 14:57:32 +0100

    Update voice.rb

--- voice.rb
+++ voice.rb
@@ -159,29 +159,44 @@ module Sidtool
       # ...
     end
  
-    def convert_attack(attack)
-      # Conversion based on SID 6581 specifications
-      # Implement SID's ADSR conversion for attack
-      case attack
-      when 0 then 0.002
-      when 1 then 0.008
-      when 2 then 0.016
-      when 3 then 0.024
-      when 4 then 0.038
-      when 5 then 0.056
-      when 6 then 0.068
-      when 7 then 0.08
-      when 8 then 0.1
-      when 9 then 0.25
-      when 10 then 0.5
-      when 11 then 0.8
-      when 12 then 1
-      when 13 then 3
-      when 14 then 5
-      when 15 then 8
-      else raise "Unknown value: #{attack}"
-      end
-    end
+def convert_attack(attack)
+  # Converts the SID's attack rate value (0-15) to a corresponding time duration.
+  # These durations determine how quickly the sound reaches its peak amplitude from silence.
+  # The values provided here are based on the SID 6581 chip's specifications and represent
+  # the duration of the attack phase in seconds.
+  #
+  # attack - The attack rate value from the SID chip, ranging from 0 to 15.
+  #
+  # The method returns a float representing the duration in seconds for the attack phase.
+  # For example, a value of 0.002 means the attack phase will last 2 milliseconds.
+  #
+  # It is important to adjust these values to match the real behavior of the SID model being emulated,
+  # as different models or revisions of the SID chip might have slightly different ADSR timings.
+  #
+  # The method raises an exception if an unknown attack value is provided, ensuring that
+  # only valid SID attack values are processed.
+  case attack
+  when 0 then 0.002  # Fastest attack, 2 milliseconds
+  when 1 then 0.008
+  when 2 then 0.016
+  when 3 then 0.024
+  when 4 then 0.038
+  when 5 then 0.056
+  when 6 then 0.068
+  when 7 then 0.08
+  when 8 then 0.1   # 100 milliseconds
+  when 9 then 0.25  # Quarter of a second
+  when 10 then 0.5  # Half a second
+  when 11 then 0.8
+  when 12 then 1    # One second
+  when 13 then 3    # Three seconds
+  when 14 then 5    # Five seconds
+  when 15 then 8    # Slowest attack, eight seconds
+  else
+    raise "Unknown attack value: #{attack}"
+  end
+end
+
 
     def convert_decay_or_release(decay_or_release)
       # Conversion based on SID 6581 specifications
