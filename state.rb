d5ef0f2718ea8b2ee2b23285255625c6f2121f5c
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-16 02:29:29 +0100

    Update state.rb

--- state.rb
+++ state.rb
@@ -1,54 +1,140 @@
 module Sidtool
-  class State
-    # Accessors for the current frame and readers for SID and CIA timers.
-    # The current frame represents the current time step in the emulation.
-    attr_accessor :current_frame
-    attr_reader :sid6581
-    attr_reader :cia_timers
+  # Class representing a CIA timer, typically used in emulations
+  class CIATimer
+    # Constants for control register flags
+    START_FLAG = 0x01       # Timer start/stop
+    ONESHOT_FLAG = 0x08     # One-shot mode
+    PHASE_FLAG = 0x10       # Timer counts system clocks (PHI2) or underflows from other timer
+    TOGGLE_FLAG = 0x40      # Timer output toggles between high and low on underflow
+    INTERRUPT_FLAG = 0x80   # Enable interrupt on timer underflow
 
-    # Initialize the state with default values.
-    # This includes setting the current frame to zero, initializing the SID6581 object for SID emulation,
-    # and creating two CIA timers for managing timing-related functions.
+    attr_accessor :timer, :latch, :control_register, :underflow, :output_pin
+
+    # Initialize the CIA timer with a reference to the CPU
+    def initialize(cpu)
+      @cpu = cpu
+      reset
+    end
+
+    # Reset the timer to its initial state
+    def reset
+      @timer = 0xFFFF
+      @latch = 0xFFFF
+      @control_register = 0
+      @underflow = false
+      @output_pin = false
+    end
+
+    # Update the timer's state based on control flags
+    def update
+      if running?
+        decrement_timer
+        check_underflow
+      end
+    end
+
+    # Decrement the timer based on the phase flag
+    def decrement_timer
+      if (@control_register & PHASE_FLAG) != 0
+        @timer -= 1 if @timer > 0
+      else
+        decrement_on_external_event if external_event_occurred?
+      end
+    end
+
+    # Handle the timer's underflow logic
+    def handle_underflow
+      @underflow = true
+      toggle_output if (@control_register & TOGGLE_FLAG) != 0
+      trigger_interrupt if (@control_register & INTERRUPT_FLAG) != 0
+    end
+
+    # Check and handle timer underflow
+    def check_underflow
+      if @timer == 0
+        handle_underflow
+        reload_timer if should_reload?
+      end
+    end
+
+    # Reload the timer from the latch
+    def reload_timer
+      @timer = @latch
+    end
+
+    # Toggle the output pin state
+    def toggle_output
+      @output_pin = !@output_pin
+    end
+
+    # Trigger an interrupt on the CPU
+    def trigger_interrupt
+      @cpu.trigger_interrupt(:cia_timer_underflow)
+    end
+
+    # Check if the timer is currently running
+    def running?
+      (@control_register & START_FLAG) != 0
+    end
+
+    # Determine if the timer should reload after underflow
+    def should_reload?
+      (@control_register & ONESHOT_FLAG) == 0 || !@underflow
+    end
+
+    # Set the lower byte of the latch
+    def set_latch_lo(value)
+      @latch = (@latch & 0xFF00) | value
+    end
+
+    # Set the higher byte of the latch
+    def set_latch_hi(value)
+      @latch = (@latch & 0x00FF) | (value << 8)
+    end
+
+    # Set the control register value
+    def set_control_register(value)
+      @control_register = value
+    end
+
+    private
+
+    # Decrement the timer based on external events (Placeholder method)
+    def decrement_on_external_event
+      # Placeholder logic for external event decrement
+      if external_event_occurred?
+        @timer -= 1 if @timer > 0
+      end
+    end
+
+    # Check if an external event occurred (Placeholder method)
+    def external_event_occurred?
+      # Placeholder logic for external event check
+      check_external_event_source
+    end
+
+    # Check the source of external events (Placeholder method)
+    def check_external_event_source
+      # Placeholder logic for external event source check
+      false
+    end
+  end
+
+  # Class representing the state of the emulation
+  class State
+    # Accessors for current frame and readers for SID and CIA timers
+    attr_accessor :current_frame
+    attr_reader :sid6581, :cia_timers
+
+    # Initialize the state with default values
     def initialize
       @current_frame = 0
       @sid6581 = Sid6581.new
       @cia_timers = [CIATimer.new(self), CIATimer.new(self)]
     end
 
-    # Update the state for the current frame.
-    # This method advances the emulation by one frame, updating timers, handling interrupts,
-    # and updating the SID chip's state.
+    # Update the state for the current frame
     def update
-      # Update each CIA timer.
       @cia_timers.each(&:update)
-
-      # Handle any interrupts that might have occurred, which could affect the SID's state or other emulation aspects.
       handle_interrupts
-
-      # Update the state of the SID6581 chip, typically involving sound generation or processing for the current frame.
-      @sid6581.update
-
-      # Increment the current frame, advancing the emulation time.
-      @current_frame += 1
-    end
-
-    private
-
-    # Handle interrupts from CIA timers or other sources.
-    # This private method is responsible for managing any interrupts or special conditions
-    # that arise, particularly those related to timer underflows.
-    def handle_interrupts
-      @cia_timers.each do |timer|
-        if timer.underflow
-          # Specific actions to be taken on timer underflow, such as triggering interrupts,
-          # updating states, or other responses specific to the emulation context.
-        end
-      end
-
-      # Additional logic for handling interrupts from other sources or complex state changes can be placed here.
-    end
-
-    # Additional methods related to state management can be added here.
-    # For example, methods for synchronizing with external components, handling user input, etc.
-  end
-end
+      @sid
