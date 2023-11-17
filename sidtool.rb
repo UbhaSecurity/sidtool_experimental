6c7aa9468b932aa31db8598748c505e93740e599
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-17 19:03:56 +0100

    Update sidtool.rb

--- sidtool.rb
+++ sidtool.rb
@@ -12,13 +12,10 @@ require 'mos6510'
 
 module Sidtool
   # Define constants for PAL properties.
-  # FRAMES_PER_SECOND sets the frame rate for the emulation, specific to the PAL system.
-  # CLOCK_FREQUENCY sets the clock frequency of the SID chip in the PAL system.
   FRAMES_PER_SECOND = 50.0
   CLOCK_FREQUENCY = 985248.0
 
   # Constants for slide detection and handling in the emulation.
-  # SLIDE_THRESHOLD and SLIDE_DURATION_FRAMES can be adjusted as needed for specific emulation requirements.
   SLIDE_THRESHOLD = 60
   SLIDE_DURATION_FRAMES = 20
 
@@ -27,15 +24,14 @@ module Sidtool
 
   # SidWrapper class to interface with the SID chip and CIA timers.
   class SidWrapper
-    # Initialize the SidWrapper with a SID6581 object and two CIATimer instances.
+    attr_reader :sid6581, :ciaTimerA, :ciaTimerB
+
     def initialize
       @sid6581 = Sid6581.new
       @ciaTimerA = CIATimer.new(STATE)
       @ciaTimerB = CIATimer.new(STATE)
     end
 
-    # Method to write values to the SID chip's registers.
-    # Handles specific address ranges for the SID chip.
     def poke(address, value)
       case address
       when 0xD400..0xD41C
@@ -45,32 +41,26 @@ module Sidtool
       end
     end
 
-    # Method to emulate a single cycle of the SID chip.
-    # Updates CIA timers and generates sound for the current cycle.
     def emulate_cycle
-      @ciaTimerA.update
-      @ciaTimerB.update
+      STATE.update
       @sid6581.generate_sound
     end
   end
 
   # Class method to initialize SID chip emulation.
-  # Sets up the SidWrapper and Mos6510 CPU for the emulation process.
   def self.initialize_sid_emulation
     @sid_wrapper = SidWrapper.new
     @cpu = Mos6510::Cpu.new(sid: @sid_wrapper)
   end
 
   # Class method to run the emulation loop.
-  # Continuously emulates SID cycles, can include additional logic for the emulation process.
   def self.emulate
     loop do
       @sid_wrapper.emulate_cycle
-      # Place for additional logic for the emulation loop, if required.
+      # Additional logic for the emulation loop, if required.
     end
   end
 end
 
 # Initialize the SID emulation setup by calling the class method.
 Sidtool.initialize_sid_emulation
-
