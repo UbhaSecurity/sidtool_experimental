3aa43f8c0d8aee49f6c7b1795bd2f8d88a8f8682
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-18 21:25:14 +0100

    Update Mos6510.rb

--- lib/sidtool_experimental/Mos6510.rb
+++ lib/sidtool_experimental/Mos6510.rb
@@ -426,5 +426,5 @@ end
  0x11D => { operation: method(:sbc), addr_mode: Mode::ABX, cycles: 4 },
  0x11E => { operation: method(:inc), addr_mode: Mode::ABX, cycles: 7 },
  0x11F => { operation: method(:sbc), addr_mode: Mode::ABX, cycles: 7 }
-} # This is the closing brace for the INSTRUCTIONS hash
-# No 'end' needed here
+}
+  end
