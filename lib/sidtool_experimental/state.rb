851da03cb342bf4a0a6176df51105447816b1e12
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-21 17:45:52 +0100

    Update state.rb

--- lib/sidtool_experimental/state.rb
+++ lib/sidtool_experimental/state.rb
@@ -1,127 +1,4 @@
 module SidtoolExperimental
-  # Class representing a CIA timer, typically used in emulations
-  class CIATimer
-    # Constants for control register flags
-    START_FLAG = 0x01       # Timer start/stop
-    ONESHOT_FLAG = 0x08     # One-shot mode
-    PHASE_FLAG = 0x10       # Timer counts system clocks (PHI2) or underflows from other timer
-    TOGGLE_FLAG = 0x40      # Timer output toggles between high and low on underflow
-    INTERRUPT_FLAG = 0x80   # Enable interrupt on timer underflow
-
-    attr_accessor :timer, :latch, :control_register, :underflow, :output_pin, :current_frame, :sid6581, :cia_timers, :cpu
-
-    def initialize(cpu)
-      @cpu = cpu # Store the reference to the CPU
-      @current_frame = 0
-      @sid6581 = Sid6581.new
-      @cia_timers = [CIATimer.new(@cpu), CIATimer.new(@cpu)] # Pass CPU reference to CIATimer
-    end
-
-    # Reset the timer to its initial state
-    def reset
-      @timer = 0xFFFF
-      @latch = 0xFFFF
-      @control_register = 0
-      @underflow = false
-      @output_pin = false
-    end
-
-    # Update the timer's state based on control flags
-    def update
-      if running?
-        decrement_timer
-        check_underflow
-      end
-    end
-
-    # Decrement the timer based on the phase flag
-    def decrement_timer
-      if (@control_register & PHASE_FLAG) != 0
-        @timer -= 1 if @timer > 0
-      else
-        decrement_on_external_event if external_event_occurred?
-      end
-    end
-
-    # Handle the timer's underflow logic
-    def handle_underflow
-      @underflow = true
-      toggle_output if (@control_register & TOGGLE_FLAG) != 0
-      trigger_interrupt if (@control_register & INTERRUPT_FLAG) != 0
-    end
-
-    # Check and handle timer underflow
-    def check_underflow
-      if @timer == 0
-        handle_underflow
-        reload_timer if should_reload?
-      end
-    end
-
-    # Reload the timer from the latch
-    def reload_timer
-      @timer = @latch
-    end
-
-    # Toggle the output pin state
-    def toggle_output
-      @output_pin = !@output_pin
-    end
-
-    # Trigger an interrupt on the CPU
-    def trigger_interrupt
-      @cpu.trigger_interrupt(:cia_timer_underflow)
-    end
-
-    # Check if the timer is currently running
-    def running?
-      (@control_register & START_FLAG) != 0
-    end
-
-    # Determine if the timer should reload after underflow
-    def should_reload?
-      (@control_register & ONESHOT_FLAG) == 0 || !@underflow
-    end
-
-    # Set the lower byte of the latch
-    def set_latch_lo(value)
-      @latch = (@latch & 0xFF00) | value
-    end
-
-    # Set the higher byte of the latch
-    def set_latch_hi(value)
-      @latch = (@latch & 0x00FF) | (value << 8)
-    end
-
-    # Set the control register value
-    def set_control_register(value)
-      @control_register = value
-    end
-
-    private
-
-    # Decrement the timer based on external events (Placeholder method)
-    def decrement_on_external_event
-      # Placeholder logic for external event decrement
-      if external_event_occurred?
-        @timer -= 1 if @timer > 0
-      end
-    end
-
-    # Check if an external event occurred (Placeholder method)
-    def external_event_occurred?
-      # Placeholder logic for external event check
-      check_external_event_source
-    end
-
-    # Check the source of external events (Placeholder method)
-    def check_external_event_source
-      # Placeholder logic for external event source check
-      false
-    end
-  end
-
-  # Class representing the state of the emulation
   class State
     attr_accessor :current_frame
     attr_reader :sid6581, :cia_timers
@@ -148,16 +25,12 @@ module SidtoolExperimental
     def handle_timer_events
       @cia_timers.each do |timer|
         if timer.underflow
-          # Here, handle the specific actions on timer underflow.
-          # For example, trigger sound changes in SID or update other components.
-          # Example: sid6581.trigger_sound(timer.some_parameter) if timer.underflow_condition?
+          # Handle specific actions on timer underflow
         end
       end
     end
 
     def update_sid
-      # This is where the Sid6581 and its related components (like Voice and Synth) are updated.
-      # Ensure that they use the current state, especially the current_frame for synchronization.
       @sid6581.update(current_frame)
     end
 
