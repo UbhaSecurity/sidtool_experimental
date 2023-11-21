d0c1b66e1ffc707942ec7f76b8c8fe5960ab3520
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-21 17:54:52 +0100

    Update state.rb

--- lib/sidtool_experimental/state.rb
+++ lib/sidtool_experimental/state.rb
@@ -1,12 +1,13 @@
 module SidtoolExperimental
   class State
-    attr_accessor :current_frame
+    attr_accessor :current_frame, :emulation_finished
     attr_reader :sid6581, :cia_timers
 
     def initialize
       @current_frame = 0
       @sid6581 = Sid6581.new
       @cia_timers = [CIATimer.new(self), CIATimer.new(self)]
+      @emulation_finished = false
     end
 
     def update
@@ -14,6 +15,12 @@ module SidtoolExperimental
       handle_timer_events
       update_sid
       increment_frame
+    end
+
+    def handle_interrupts
+      # Logic to handle interrupts (e.g., from CIA timers, IRQ, NMI)
+      # This might involve checking the state of the CIA timers and triggering
+      # CPU interrupts as needed.
     end
 
     private
@@ -25,12 +32,14 @@ module SidtoolExperimental
     def handle_timer_events
       @cia_timers.each do |timer|
         if timer.underflow
-          # Handle specific actions on timer underflow
+          # Handle actions on timer underflow
+          # Example: sid6581.trigger_sound(timer.some_parameter) if timer.underflow_condition?
         end
       end
     end
 
     def update_sid
+      # Update the Sid6581 chip state
       @sid6581.update(current_frame)
     end
 
