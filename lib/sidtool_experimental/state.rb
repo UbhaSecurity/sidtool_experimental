96a204d121ed091c8dbe84f170802daac37b9405
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-22 09:23:37 +0100

    Update state.rb

--- lib/sidtool_experimental/state.rb
+++ lib/sidtool_experimental/state.rb
@@ -3,48 +3,74 @@ module SidtoolExperimental
     attr_accessor :current_frame, :emulation_finished
     attr_reader :sid6581, :cia_timers
 
+     class State
+    attr_accessor :current_frame, :emulation_finished
+    attr_reader :sid6581, :cia_timers, :irq_vector, :nmi_vector, :break_vector
+
     def initialize
       @current_frame = 0
       @sid6581 = Sid6581.new
       @cia_timers = [CIATimer.new(self), CIATimer.new(self)]
       @emulation_finished = false
+      @interrupt_flag = false # Flag to ignore or respond to IRQs
+      initialize_vectors
     end
 
     def update
       update_timers
       handle_timer_events
       update_sid
+      handle_interrupts
       increment_frame
     end
 
     def handle_interrupts
-      # Logic to handle interrupts (e.g., from CIA timers, IRQ, NMI)
-      # This might involve checking the state of the CIA timers and triggering
-      # CPU interrupts as needed.
+      handle_irq if irq_pending? && !@interrupt_flag
+      handle_nmi if nmi_pending?
     end
 
     private
 
-    def update_timers
+ def update_timers
       @cia_timers.each(&:update)
     end
 
     def handle_timer_events
-      @cia_timers.each do |timer|
-        if timer.underflow
-          # Handle actions on timer underflow
-          # Example: sid6581.trigger_sound(timer.some_parameter) if timer.underflow_condition?
-        end
-      end
+      # Logic for timer events
     end
 
     def update_sid
-      # Update the Sid6581 chip state
       @sid6581.update(current_frame)
     end
 
     def increment_frame
       @current_frame += 1
     end
-  end
+
+    def initialize_vectors
+      # Initialize IRQ, NMI, and BREAK vectors based on KERNAL ROM
+      @irq_vector = 0xEA31
+      @nmi_vector = 0xFE43
+      @break_vector = 0xFE66
+    end
+
+    def handle_irq
+      # Logic to handle IRQs
+      # Save CPU state, execute IRQ routine, and restore CPU state
+      # Jump to the IRQ vector address
+    end
+
+    def handle_nmi
+      # Logic to handle NMIs
+      # Similar to IRQ but cannot be ignored
+      # Jump to the NMI vector address
+    end
+
+    def irq_pending?
+      # Logic to determine if an IRQ is pending
+    end
+
+    def nmi_pending?
+      # Logic to determine if an NMI is pending
+    end
 end
