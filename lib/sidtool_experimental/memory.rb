fd52914c5b618600a7e06cd4938ef182a1091358
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-22 14:01:48 +0100

    Update memory.rb

--- lib/sidtool_experimental/memory.rb
+++ lib/sidtool_experimental/memory.rb
@@ -6,37 +6,68 @@ class Memory
     @ram = Array.new(65536, 0) # 64KB of RAM
     @basic_rom = load_rom('basic.rom') # Load BASIC ROM
     @kernal_rom = load_rom('kernal.rom') # Load KERNAL ROM
-    @char_rom = load_rom('char.rom') # Load Character ROM
+    @char_rom = load_rom('character.rom') # Load Character ROM
     @io_devices = setup_io_devices # Setup I/O devices (VIC, SID, CIA)
 
     # Memory management attributes
     @loram = @hiram = @charen = 1 # Defaults to 1 on reset
     @exrom = @game = 0            # Defaults to 0 (no cartridge)
+    @processor_port = 0x37       # Default value for processor port
+    @pla_state = {}              # Placeholder for PLA state, use actual data
   end
+end
 
-  def read(address)
+
+ def read(address)
+    config = current_memory_config
+
     case address
     when 0xA000..0xBFFF
-      rom_area_basic(address)
+      return @basic_rom[address - 0xA000] if config[:basic_rom_enabled]
     when 0xD000..0xDFFF
-      io_or_char_rom(address)
+      return read_io_or_char_rom(address, config) if config[:io_enabled] || config[:char_rom_enabled]
     when 0xE000..0xFFFF
-      rom_area_kernal(address)
-    else
-      @ram[address]
+      return @kernal_rom[address - 0xE000] if config[:kernal_rom_enabled]
     end
+
+    return @ram[address] unless config[:ultimax_mode] && address.between?(0x1000, 0xCFFF)
+    nil # Open address space in Ultimax mode
   end
 
   def write(address, value)
+    config = current_memory_config
+
     case address
     when 0xD000..0xDFFF
-      write_io_or_char_rom(address, value)
+      write_io_devices(address, value) if config[:io_enabled]
+    when 0xA000..0xBFFF, 0xE000..0xFFFF
+      # Ignore writes to ROM areas
     else
-      @ram[address] = value
+      @ram[address] = value unless config[:ultimax_mode] && address.between?(0x1000, 0xCFFF)
     end
   end
 
   private
+
+  def initialize_vic_registers
+    Array.new(64, 0) # VIC-II has 64 registers, initialized to 0
+  end
+
+  def initialize_sid_registers
+    Array.new(29, 0) # SID has 29 registers, initialized to 0
+  end
+
+  def initialize_cia_registers
+    Array.new(16, 0) # Each CIA has 16 registers, initialized to 0
+  end
+
+
+# Load ROM data from binary files
+  def load_rom(filename)
+    # Load ROM data from file
+    # Replace this with your specific implementation
+    File.binread(filename).bytes
+  end
 
   def rom_area_basic(address)
     return @basic_rom[address - 0xA000] if @loram == 1 && @hiram == 1 && @exrom == 0
@@ -69,10 +100,89 @@ class Memory
     File.binread(filename).bytes
   end
 
+def address_to_vic_ii_register_index(address)
+    # Map addresses to VIC-II register indices
+    case address
+    when 0xD000
+      return 0
+    when 0xD001
+      return 1
+    # Continue with the mapping for other VIC-II registers
+    when 0xD02E
+      return 46
+    when 0xD02F
+      return 47
+    else
+      # Handle unsupported VIC-II register address
+      raise "Unsupported VIC-II register address: #{address.to_s(16)}"
+    end
+  end
+
+ # Example implementation of read_vic_ii_register
+  def read_vic_ii_register(address)
+    # Translate the address to a register index
+    register_index = address_to_vic_ii_register_index(address)
+
+    # Access VIC-II registers
+    @io_devices[:vic][register_index]
+  end
+
+  # Implement write_vic_ii_register and other register access methods similarly
+  def write_vic_ii_register(address, value)
+    # Translate the address to a register index
+    register_index = address_to_vic_ii_register_index(address)
+
+    # Access VIC-II registers and write the value
+    @io_devices[:vic][register_index] = value
+  end
+
+  # Implement read_cia1_register and other CIA #1 register access methods similarly
+  def read_cia1_register(address)
+    # Translate the address to a register index for CIA #1
+    register_index = address_to_cia1_register_index(address)
+
+    # Access CIA #1 registers
+    @io_devices[:cia1][register_index]
+  end
+
+  def write_cia1_register(address, value)
+    # Translate the address to a register index for CIA #1
+    register_index = address_to_cia1_register_index(address)
+
+    # Access CIA #1 registers and write the value
+    @io_devices[:cia1][register_index] = value
+  end
+
+  # Implement read_cia2_register and other CIA #2 register access methods similarly
+  def read_cia2_register(address)
+    # Translate the address to a register index for CIA #2
+    register_index = address_to_cia2_register_index(address)
+
+    # Access CIA #2 registers
+    @io_devices[:cia2][register_index]
+  end
+
+  def write_cia2_register(address, value)
+    # Translate the address to a register index for CIA #2
+    register_index = address_to_cia2_register_index(address)
+
+    # Access CIA #2 registers and write the value
+    @io_devices[:cia2][register_index] = value
+  end
+
+  # Setup I/O devices
   def setup_io_devices
     {
-      # Initialize and set up VIC-II, SID, CIA, etc.
-      # Example: vic: VICII.new, sid: SID.new, cia1: CIA.new, cia2: CIA.new
+      # Initialize VIC-II registers (for video interface)
+      vic: initialize_vic_registers,
+
+      # Initialize SID registers (for sound interface)
+      sid: initialize_sid_registers,
+
+      # Initialize CIA #1 and CIA #2 registers (for various interface tasks)
+      cia1: initialize_cia_registers,
+      cia2: initialize_cia_registers,
+
       # Add other devices as needed
     }
   end
