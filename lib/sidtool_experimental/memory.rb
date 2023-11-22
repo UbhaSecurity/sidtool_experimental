bd602f9bd28f11bed0fd065959c5ad298b11a59d
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-22 13:48:36 +0100

    Update memory.rb

--- lib/sidtool_experimental/memory.rb
+++ lib/sidtool_experimental/memory.rb
@@ -1,340 +1,79 @@
-module SidtoolExperimental
-  class Memory
-    def initialize
-      @ram = Array.new(65536, 0)  # 64KB of RAM
-      @rom = load_roms             # Load ROM data
-      @io_registers = initialize_io_registers # Initialize I/O registers
-      @processor_port = 0x37      # Default value for processor port
-      @pla_state = {}             # Placeholder for PLA state, use actual data
-    end
+class Memory
+  attr_accessor :ram, :basic_rom, :kernal_rom, :char_rom, :io_devices
+  attr_accessor :loram, :hiram, :charen, :exrom, :game
 
-    def read(address)
-      case address
-      when 0xA000..0xBFFF
-        rom_is_mapped?(address) ? @rom['BASIC'][address - 0xA000] : @ram[address]
-      when 0xD000..0xDFFF
-        io_area?(address) ? read_io(address) : (char_rom_mapped?(address) ? @rom['CHAR'][address - 0xD000] : @ram[address])
-      when 0xE000..0xFFFF
-        rom_is_mapped?(address) ? @rom['KERNAL'][address - 0xE000] : @ram[address]
-      else
-        @ram[address]
-      end
-    end
+  def initialize
+    @ram = Array.new(65536, 0) # 64KB of RAM
+    @basic_rom = load_rom('basic.rom') # Load BASIC ROM
+    @kernal_rom = load_rom('kernal.rom') # Load KERNAL ROM
+    @char_rom = load_rom('char.rom') # Load Character ROM
+    @io_devices = setup_io_devices # Setup I/O devices (VIC, SID, CIA)
 
-    def write(address, value)
-      case address
-      when 0x0000
-        @processor_port_direction = value
-      when 0x0001
-        @processor_port = value
-      when 0xA000..0xBFFF, 0xE000..0xFFFF
-        @ram[address] = value unless rom_is_mapped?(address)
-      when 0xD000..0xDFFF
-        write_io(address, value) if io_area?(address)
-      else
-        @ram[address] = value
-      end
-    end
-
-  # Load ROM data from binary files
-  def load_roms
-    rom_data = {}
-
-    # Load BASIC ROM from a binary file
-    basic_rom_filename = 'basic.rom'  # Replace with the actual filename
-    rom_data['BASIC'] = File.binread(basic_rom_filename)
-
-    # Load KERNAL ROM from a binary file
-    kernal_rom_filename = 'kernal.rom'  # Replace with the actual filename
-    rom_data['KERNAL'] = File.binread(kernal_rom_filename)
-
-    # Load Character ROM from a binary file
-    character_rom_filename = 'character.rom'  # Replace with the actual filename
-    rom_data['CHAR'] = File.binread(character_rom_filename)
-
-    rom_data
+    # Memory management attributes
+    @loram = @hiram = @charen = 1 # Defaults to 1 on reset
+    @exrom = @game = 0            # Defaults to 0 (no cartridge)
   end
 
-  # Initialize I/O registers
-    def initialize_io_registers
-      io_registers = {}
-
-      # Initialize VIC-II registers
-      io_registers[:vic_registers] = Array.new(64, 0)
-
-      # Initialize SID registers
-      io_registers[:sid_registers] = Array.new(29, 0)
-
-      # Initialize CIA registers (e.g., CIA1 and CIA2)
-      io_registers[:cia1_registers] = Array.new(16, 0)
-      io_registers[:cia2_registers] = Array.new(16, 0)
-
-      # You may have more I/O registers for other components
-
-      return io_registers
-    end
-
-  # Check if the ROM is mapped at the address
-  def rom_is_mapped(address)
-    # Example simplified logic:
-    # If the PLA's AEC (Address Enable for CPU) signal is active,
-    # then the ROM is not mapped, otherwise, it is mapped.
-
-    # Assuming you have an instance variable @pla_state that represents
-    # the current state of the PLA, and AEC is one of its signals.
-    # Adjust this based on your actual emulation logic.
-
-    !@pla_state[:aec]
-  end
-
-  # Check if address is in I/O area
-  def io_area?(address)
-    (0xD000..0xDFFF).include?(address)
-  end
-
-   # Read from I/O registers
-     def read_io(address)
-      case address
-      when 0xD000..0xD3FF  # VIC-II range
-        # Handle VIC-II read operations
-        return read_vic_ii_register(address)
-      when 0xDC00..0xDCFF  # CIA #1 range
-        # Handle CIA #1 read operations
-        return read_cia1_register(address)
-      when 0xDD00..0xDDFF  # CIA #2 range
-        # Handle CIA #2 read operations
-        return read_cia2_register(address)
-      else
-    when 0xD400
-      @sid6581.read_register(Sid6581::FRELO1)
-    when 0xD401
-      @sid6581.read_register(Sid6581::FREHI1)
-    when 0xD402
-      @sid6581.read_register(Sid6581::PWLO1)
-    when 0xD403
-      @sid6581.read_register(Sid6581::PWHI1)
-    when 0xD404
-      @sid6581.read_register(Sid6581::VCREG1)
-    when 0xD405
-      @sid6581.read_register(Sid6581::ATDCY1)
-    when 0xD406
-      @sid6581.read_register(Sid6581::SUREL1)
-    when 0xD407
-      @sid6581.read_register(Sid6581::FRELO2)
-    when 0xD408
-      @sid6581.read_register(Sid6581::FREHI2)
-    when 0xD409
-      @sid6581.read_register(Sid6581::PWLO2)
-    when 0xD40A
-      @sid6581.read_register(Sid6581::PWHI2)
-    when 0xD40B
-      @sid6581.read_register(Sid6581::VCREG2)
-    when 0xD40C
-      @sid6581.read_register(Sid6581::ATDCY2)
-    when 0xD40D
-      @sid6581.read_register(Sid6581::SUREL2)
-    when 0xD40E
-      @sid6581.read_register(Sid6581::FRELO3)
-    when 0xD40F
-      @sid6581.read_register(Sid6581::FREHI3)
-    when 0xD410
-      @sid6581.read_register(Sid6581::PWLO3)
-    when 0xD411
-      @sid6581.read_register(Sid6581::PWHI3)
-    when 0xD412
-      @sid6581.read_register(Sid6581::VCREG3)
-    when 0xD413
-      @sid6581.read_register(Sid6581::ATDCY3)
-    when 0xD414
-      @sid6581.read_register(Sid6581::SUREL3)
-    when 0xD415
-      @sid6581.read_register(Sid6581::CUTLO)
-    when 0xD416
-      @sid6581.read_register(Sid6581::CUTHI)
-    when 0xD417
-      @sid6581.read_register(Sid6581::RESON)
-    when 0xD418
-      @sid6581.read_register(Sid6581::SIGVOL)
-    when 0xD419
-      @sid6581.read_register(Sid6581::POTX)
-    when 0xD41A
-      @sid6581.read_register(Sid6581::POTY)
-    when 0xD41B
-      @sid6581.read_register(Sid6581::RANDOM)
-    when 0xD41C
-      @sid6581.read_register(Sid6581::ENV3)
+  def read(address)
+    case address
+    when 0xA000..0xBFFF
+      rom_area_basic(address)
+    when 0xD000..0xDFFF
+      io_or_char_rom(address)
+    when 0xE000..0xFFFF
+      rom_area_kernal(address)
     else
-      # Handle other I/O reads here
-      raise "Unsupported I/O read at address #{address.to_s(16)}"
+      @ram[address]
     end
   end
 
-    # Write to I/O registers
-    def write_io(address, value)
-      case address
-    when 0xD000..0xD3FF  # VIC-II range
-      # Handle VIC-II write operations
-      write_vic_ii_register(address, value)
-    when 0xDC00..0xDCFF  # CIA #1 range
-      # Handle CIA #1 write operations
-      write_cia1_register(address, value)
-    when 0xDD00..0xDDFF  # CIA #2 range
-      # Handle CIA #2 write operations
-      write_cia2_register(address, value)
-    when 0xD400
-      @sid6581.write_register(Sid6581::FRELO1, value)
-    when 0xD401
-      @sid6581.write_register(Sid6581::FREHI1, value)
-    when 0xD402
-      @sid6581.write_register(Sid6581::PWLO1, value)
-    when 0xD403
-      @sid6581.write_register(Sid6581::PWHI1, value)
-    when 0xD404
-      @sid6581.write_register(Sid6581::VCREG1, value)
-    when 0xD405
-      @sid6581.write_register(Sid6581::ATDCY1, value)
-    when 0xD406
-      @sid6581.write_register(Sid6581::SUREL1, value)
-    when 0xD407
-      @sid6581.write_register(Sid6581::FRELO2, value)
-    when 0xD408
-      @sid6581.write_register(Sid6581::FREHI2, value)
-    when 0xD409
-      @sid6581.write_register(Sid6581::PWLO2, value)
-    when 0xD40A
-      @sid6581.write_register(Sid6581::PWHI2, value)
-    when 0xD40B
-      @sid6581.write_register(Sid6581::VCREG2, value)
-    when 0xD40C
-      @sid6581.write_register(Sid6581::ATDCY2, value)
-    when 0xD40D
-      @sid6581.write_register(Sid6581::SUREL2, value)
-    when 0xD40E
-      @sid6581.write_register(Sid6581::FRELO3, value)
-    when 0xD40F
-      @sid6581.write_register(Sid6581::FREHI3, value)
-    when 0xD410
-      @sid6581.write_register(Sid6581::PWLO3, value)
-    when 0xD411
-      @sid6581.write_register(Sid6581::PWHI3, value)
-    when 0xD412
-      @sid6581.write_register(Sid6581::VCREG3, value)
-    when 0xD413
-      @sid6581.write_register(Sid6581::ATDCY3, value)
-    when 0xD414
-      @sid6581.write_register(Sid6581::SUREL3, value)
-    when 0xD415
-      @sid6581.write_register(Sid6581::CUTLO, value)
-    when 0xD416
-      @sid6581.write_register(Sid6581::CUTHI, value)
-    when 0xD417
-      @sid6581.write_register(Sid6581::RESON, value)
-    when 0xD418
-      @sid6581.write_register(Sid6581::SIGVOL, value)
-    when 0xD419
-      @sid6581.write_register(Sid6581::POTX, value)
-    when 0xD41A
-      @sid6581.write_register(Sid6581::POTY, value)
-    when 0xD41B
-      @sid6581.write_register(Sid6581::RANDOM, value)
-    when 0xD41C
-      @sid6581.write_register(Sid6581::ENV3, value)
+  def write(address, value)
+    case address
+    when 0xD000..0xDFFF
+      write_io_or_char_rom(address, value)
     else
-      # Handle other I/O writes here
-      raise "Unsupported I/O write at address #{address.to_s(16)}"
+      @ram[address] = value
     end
   end
 
- # Example implementation of read_vic_ii_register
-    def read_vic_ii_register(address)
-      # Translate the address to a register index
-      register_index = address_to_vic_ii_register_index(address)
+  private
 
-      # Access VIC-II registers
-      @io_registers[:vic_registers][register_index]
-    end
-
-    # Implement write_vic_ii_register and other register access methods similarly
-    def write_vic_ii_register(address, value)
-      # Translate the address to a register index
-      register_index = address_to_vic_ii_register_index(address)
-
-      # Access VIC-II registers and write the value
-      @io_registers[:vic_registers][register_index] = value
-    end
-
-    # Implement read_cia1_register and other CIA #1 register access methods similarly
-    def read_cia1_register(address)
-      # Translate the address to a register index for CIA #1
-      register_index = address_to_cia1_register_index(address)
-
-      # Access CIA #1 registers
-      @io_registers[:cia1_registers][register_index]
-    end
-
-    def write_cia1_register(address, value)
-      # Translate the address to a register index for CIA #1
-      register_index = address_to_cia1_register_index(address)
-
-      # Access CIA #1 registers and write the value
-      @io_registers[:cia1_registers][register_index] = value
-    end
-
-    # Implement read_cia2_register and other CIA #2 register access methods similarly
-    def read_cia2_register(address)
-      # Translate the address to a register index for CIA #2
-      register_index = address_to_cia2_register_index(address)
-
-      # Access CIA #2 registers
-      @io_registers[:cia2_registers][register_index]
-    end
-
-    def write_cia2_register(address, value)
-      # Translate the address to a register index for CIA #2
-      register_index = address_to_cia2_register_index(address)
-
-      # Access CIA #2 registers and write the value
-      @io_registers[:cia2_registers][register_index] = value
-    end
-
-    # Implement other helper methods if needed
-    # For example, address_to_vic_ii_register_index, address_to_cia1_register_index,
-    # address_to_cia2_register_index, and others as required for your emulation
-
-private
-
-  def address_to_vic_ii_register_index(address)
-      # Implement your logic to map addresses to VIC-II register indices
-      # Example: return the register index based on the address range
-      case address
-      when 0xD000
-        return 0
-      when 0xD001
-        return 1
-      # ... (Continue with the mapping for other VIC-II registers)
-      else
-        # Handle unsupported VIC-II register address
-        raise "Unsupported VIC-II register address: #{address.to_s(16)}"
-      end
-    end
-
-def rom_is_mapped?(address)
-    case @processor_port & 0x07
-    when 0x00, 0x01, 0x10
-      false  # RAM visible
-    when 0x11
-      true   # BASIC ROM at 0xA000-0xBFFF, KERNAL ROM at 0xE000-0xFFFF
-    else
-      # Default or other cases
-      false
-    end
+  def rom_area_basic(address)
+    return @basic_rom[address - 0xA000] if @loram == 1 && @hiram == 1 && @exrom == 0
+    @ram[address]
   end
 
-  def char_rom_mapped?(address)
-    (@processor_port & 0x04).zero? && (0xD000..0xDFFF).include?(address)
+  def io_or_char_rom(address)
+    if @charen == 1 && @hiram == 1 && @exrom == 0
+      return @io_devices.read_io(address)
+    elsif @charen == 0
+      return @char_rom[address - 0xD000]
+    end
+    @ram[address]
   end
 
-  def io_area?(address)
-    (@processor_port & 0x04) != 0 && (0xD000..0xDFFF).include?(address)
+  def rom_area_kernal(address)
+    return @kernal_rom[address - 0xE000] if @hiram == 1
+    @ram[address]
+  end
+
+  def write_io_or_char_rom(address, value)
+    if @charen == 1 && @hiram == 1 && @exrom == 0
+      @io_devices.write_io(address, value)
+    end
+    # Ignore writes to ROM areas and char ROM
+  end
+
+  def load_rom(filename)
+    # Implement ROM loading logic here
+    File.binread(filename).bytes
+  end
+
+  def setup_io_devices
+    {
+      # Initialize and set up VIC-II, SID, CIA, etc.
+      # Example: vic: VICII.new, sid: SID.new, cia1: CIA.new, cia2: CIA.new
+      # Add other devices as needed
+    }
   end
 end
