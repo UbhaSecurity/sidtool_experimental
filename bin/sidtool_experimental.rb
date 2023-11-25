e3dbabf85e671cc877c27cb49b423c4c7e6d0879
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-25 23:21:10 +0100

    Update sidtool_experimental.rb

--- bin/sidtool_experimental.rb
+++ bin/sidtool_experimental.rb
@@ -44,7 +44,7 @@ module SidtoolExperimental
   end
 
 # Assuming you have a CPU instance available at this point in your code
-cpu_instance = Mos6510::Cpu.new(some_memory_instance)
+# cpu_instance = Mos6510::Cpu.new(some_memory_instance)
 STATE = State.new(cpu_instance)
 
 
