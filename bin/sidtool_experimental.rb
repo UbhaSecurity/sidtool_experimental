a1577f51d9a6557bf10eeadc2f1b7b5dc3965551
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-25 21:41:08 +0100

    Update sidtool_experimental.rb

--- bin/sidtool_experimental.rb
+++ bin/sidtool_experimental.rb
@@ -14,23 +14,16 @@ require_relative '../lib/sidtool_experimental/CIATimer'
 require_relative '../lib/sidtool_experimental/RubyFileWriter'
 
 module SidtoolExperimental
-  # Define constants for PAL properties.
   FRAMES_PER_SECOND = 50.0
   CLOCK_FREQUENCY = 985248.0
-
-  # Constants for slide detection and handling in the emulation.
   SLIDE_THRESHOLD = 60
   SLIDE_DURATION_FRAMES = 20
-
-  # Default number of frames from Sidtool (update this value as needed)
   DEFAULT_FRAME_COUNT = 5000
 
-  # Parse command-line arguments
   def self.parse_arguments
     options = { frames: DEFAULT_FRAME_COUNT, format: 'ruby' }
     OptionParser.new do |opts|
       opts.banner = "Usage: sidtool_experimental [options] <inputfile.sid>"
-
       opts.on('-i', '--info', 'Show file information')
       opts.on('--format FORMAT', 'Output format, "ruby" or "midi"') do |format|
         options[:format] = format.downcase
@@ -50,75 +43,13 @@ module SidtoolExperimental
     options
   end
 
-  # Create a global state object for managing the overall state of the SID emulation.
   STATE = State.new
 
-  # SidWrapper class to interface with the SID chip and CIA timers.
-  class SidWrapper
-    attr_reader :sid6581, :ciaTimerA, :ciaTimerB
-
-    def initialize(sid6581_instance)
-      @memory = Memory.new  # First, initialize the Memory
-      @sid6581 = Sid6581.new(memory: @memory)  # Then create the Sid6581 instance with the Memory
-      @cpu = Mos6510::Cpu.new(memory: @memory)  # Now create the Mos6510 instance with the Memory
-      @ciaTimerA = CIATimer.new(self)  # Initialize CIA Timer A
-      @ciaTimerB = CIATimer.new(self)  # Initialize CIA Timer B
-    end
-
-    def poke(address, value)
-      case address
-      when 0xD400..0xD41C
-        @sid6581.write_register(address, value)
-      else
-        # Additional handling for other address ranges, if necessary.
-      end
-    end
-
-    def emulate_cycle
-      STATE.update
-      @sid6581.generate_sound
-    end
-  end
-
-  # Class method to initialize SID chip emulation.
-  def self.initialize_sid_emulation(sid6581_instance)
-    @sid_wrapper = SidWrapper.new(sid6581_instance)
-    @c64_emulator = C64Emulator.new(sid6581_instance)
-  end
-
-  # Class method to run the emulation loop with a specified number of frames.
-  def self.emulate(frame_limit)
-    frame_count = 0
-    loop do
-      break if frame_count >= frame_limit
-
-      @sid_wrapper.emulate_cycle
-      frame_count += 1
-
-      # Additional logic for the emulation loop, if required.
-    end
-  end
-
-  # Initialize the SID emulation setup and run the emulation loop for a given number of frames.
-  def self.run_emulation(options)
-    initialize_sid_emulation(STATE.sid6581)
-
-    if options[:info]
-      # Display file information
-      # TODO: Implement file information display logic
-    else
-      # Run SID emulation with the specified number of frames
-      emulate(options[:frames])
-    end
-  end
-
-  # Supported exporters for output format
   EXPORTERS = {
     'ruby' => RubyFileWriter,
     'midi' => MidiFileWriter
   }
 
-  # Run the SID emulation with command-line arguments.
   def self.run
     options = parse_arguments
     if ARGV.empty?
@@ -133,6 +64,28 @@ module SidtoolExperimental
       exit(1)
     end
     run_emulation(options)
+  end
+
+  def self.run_emulation(options)
+    c64_emulator = C64Emulator.new
+    c64_emulator.load_program(File.binread(input_file), 0x0801) # Example load address
+
+    if options[:info]
+      # TODO: Display file information
+    else
+      emulate(c64_emulator, options[:frames])
+    end
+  end
+
+  def self.emulate(emulator, frame_limit)
+    frame_count = 0
+    loop do
+      break if frame_count >= frame_limit
+      emulator.run
+      frame_count += 1
+    end
+    # Post-emulation tasks, like saving the output
+    exporter.export(output_file, STATE)
   end
 end
 
