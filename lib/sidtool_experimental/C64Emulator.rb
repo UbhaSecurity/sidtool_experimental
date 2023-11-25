46875d7d93ae0a387633fc166157eec2a6d5038b
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-25 23:19:27 +0100

    Update C64Emulator.rb

--- lib/sidtool_experimental/C64Emulator.rb
+++ lib/sidtool_experimental/C64Emulator.rb
@@ -1,15 +1,15 @@
 module SidtoolExperimental
   class C64Emulator
-    attr_reader :memory, :cpu, :sid6581, :ciaTimerA, :ciaTimerB
+    attr_reader :memory, :cpu, :sid6581, :ciaTimerA, :ciaTimerB, :state
 
-def initialize
-  @memory = Memory.new  # Instantiate Memory first
-  @cpu = Mos6510::Cpu.new(@memory)  # Then instantiate CPU with Memory instance
-  @sid6581 = Sid6581.new(memory: @memory)
-  @ciaTimerA = CIATimer.new(self)
-  @ciaTimerB = CIATimer.new(self)
-  @state = State.new(@cpu)  # Pass CPU instance to State
-end
+    def initialize
+      @memory = Memory.new  # Instantiate Memory first
+      @cpu = Mos6510::Cpu.new(@memory)  # Then instantiate CPU with Memory instance
+      @sid6581 = Sid6581.new(memory: @memory)
+      @ciaTimerA = CIATimer.new(self)
+      @ciaTimerB = CIATimer.new(self)
+      @state = State.new(@cpu)  # Pass CPU instance to State
+    end
 
     def load_program(program_data, start_address)
       @cpu.load_program(program_data, start_address)
