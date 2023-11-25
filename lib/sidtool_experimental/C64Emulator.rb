7a4a5846e054b5c3da4d1d0f5c5aa83a1c7fb1fd
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-25 21:41:49 +0100

    Update C64Emulator.rb

--- lib/sidtool_experimental/C64Emulator.rb
+++ lib/sidtool_experimental/C64Emulator.rb
@@ -1,17 +1,14 @@
 module SidtoolExperimental
   class C64Emulator
-    attr_reader :memory, :cpu, :sid, :state, :display, :keyboard
+    attr_reader :memory, :cpu, :sid6581, :ciaTimerA, :ciaTimerB
 
-    # Constructor now takes sid6581 instance as a parameter
-    def initialize(sid6581)
+    def initialize
       @memory = Memory.new
-      @cpu = Mos6510::Cpu.new(@memory)
-      @sid = sid6581  # Use the passed Sid6581 instance
-      @ciaTimerA = CIATimer.new(@cpu)
-      @ciaTimerB = CIATimer.new(@cpu)
+      @sid6581 = Sid6581.new(memory: @memory)
+      @cpu = Mos6510::Cpu.new(memory: @memory)
+      @ciaTimerA = CIATimer.new(self)
+      @ciaTimerB = CIATimer.new(self)
       @state = State.new
-      @display = Display.new # Placeholder for display handling
-      @keyboard = Keyboard.new # Placeholder for keyboard handling
     end
 
     def load_program(program_data, start_address)
@@ -23,8 +20,6 @@ module SidtoolExperimental
       until @state.emulation_finished?
         @cpu.step
         emulate_cycle
-        @display.refresh if @display
-        @keyboard.check_input if @keyboard
       end
     end
 
@@ -36,32 +31,10 @@ module SidtoolExperimental
 
     def emulate_cycle
       @state.update
-      @state.handle_interrupts
-      @sid.generate_sound
+      @sid6581.generate_sound
+      # Add more logic as needed
     end
 
-    def handle_sid_operations
-      # Define SID address range
-      sid_address_range = 0xD400..0xD7FF
-
-      # Iterate through SID address range
-      sid_address_range.each do |address|
-        if @memory.io_area?(address)
-          value = @memory.read_io(address)
-          @sid.write_register(address, value)
-        end
-      end
-
-      # Update SID state
-      @sid.update_sid_state
-    end
-
-    def write_register(address, value)
-      @sid.write_register(address, value)
-    end
-
-    def read_register(address)
-      @sid.read_register(address)
-    end
+    # Additional methods for SID operations, memory management, etc.
   end
 end
