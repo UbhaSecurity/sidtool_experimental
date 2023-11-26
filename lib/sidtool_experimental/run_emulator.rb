8f0d676d88814f71ed7e66d7986b62847e87014c
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-11-26 17:22:54 +0100

    Update run_emulator.rb

--- lib/sidtool_experimental/run_emulator.rb
+++ lib/sidtool_experimental/run_emulator.rb
@@ -1,5 +1,15 @@
-require_relative 'path_to_your_c64_emulator_file'
-require_relative 'path_to_your_test_c64_file'
+require_relative '../lib/sidtool_experimental/filereader'
+require_relative '../lib/sidtool_experimental/midi_file_writer'
+require_relative '../lib/sidtool_experimental/Sid6581'
+require_relative '../lib/sidtool_experimental/synth'
+require_relative '../lib/sidtool_experimental/voice'
+require_relative '../lib/sidtool_experimental/version'
+require_relative '../lib/sidtool_experimental/C64Emulator'
+require_relative '../lib/sidtool_experimental/memory'
+require_relative '../lib/sidtool_experimental/CIATimer'
+require_relative '../lib/sidtool_experimental/RubyFileWriter'
+require_relative '../lib/sidtool_experimental/state'
+require_relative '../lib/sidtool_experimental/Mos6510'
 
 test_c64 = SidtoolExperimental::TestC64.new
 test_c64.run
