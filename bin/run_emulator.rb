e75888ea4d8936c37d46453728a9bc75e83bc07f
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-04 21:28:45 +0100

    Update run_emulator.rb

--- bin/run_emulator.rb
+++ bin/run_emulator.rb
@@ -11,5 +11,22 @@ require_relative '../lib/sidtool_experimental/RubyFileWriter'
 require_relative '../lib/sidtool_experimental/state'
 require_relative '../lib/sidtool_experimental/Mos6510'
 
+module SidtoolExperimental
+  class TestC64
+    def initialize
+      @memory = Memory.new  # Initialize the complex memory system
+      @cpu = Mos6510::Cpu.new(@memory)  # Initialize the CPU with the memory system
+      # Initialize other components as needed...
+    end
+
+    def run
+      # Code to start and run the emulator goes here
+      # For instance, this could include a loop to execute CPU cycles,
+      # render graphics, process user input, etc.
+    end
+  end
+end
+
+# Main execution
 test_c64 = SidtoolExperimental::TestC64.new
 test_c64.run
