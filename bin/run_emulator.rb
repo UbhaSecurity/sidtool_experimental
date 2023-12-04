b1b919aad241b1586fa584f0e085cc48acf932aa
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-04 21:40:13 +0100

    Update run_emulator.rb

--- bin/run_emulator.rb
+++ bin/run_emulator.rb
@@ -10,25 +10,25 @@ require_relative '../lib/sidtool_experimental/CIATimer'
 require_relative '../lib/sidtool_experimental/RubyFileWriter'
 require_relative '../lib/sidtool_experimental/state'
 require_relative '../lib/sidtool_experimental/Mos6510'
-
 module SidtoolExperimental
   class TestC64
     def initialize
       @memory = Memory.new  # Initialize the complex memory system
       puts "Memory initialized: #{@memory != nil}"  # Debugging statement
 
+      # Check if memory responds to expected methods (additional debugging)
+      if @memory.respond_to?(:[]) && @memory.respond_to?(:[]=)
+        puts "Memory object is valid and ready."
+      else
+        puts "Memory object is not valid. Please check Memory class implementation."
+      end
+
       @cpu = Mos6510::Cpu.new(@memory)  # Initialize the CPU with the memory system
-      # Initialize other components as needed...
+      puts "CPU initialized with memory."  # Debugging statement
     end
 
     def run
-      # Code to start and run the emulator goes here
-      # For instance, this could include a loop to execute CPU cycles,
-      # render graphics, process user input, etc.
+      # Implementation for running the emulator...
     end
   end
 end
-
-# Main execution
-test_c64 = SidtoolExperimental::TestC64.new
-test_c64.run
