155832363ebaaefdd67070d9300acec0fd4f198c
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-05 22:12:30 +0100

    Update C64Emulator.rb

--- lib/sidtool_experimental/C64Emulator.rb
+++ lib/sidtool_experimental/C64Emulator.rb
@@ -1,81 +1,75 @@
 module SidtoolExperimental
-class C64Emulator
-  attr_reader :memory, :cpu, :ciaTimerA, :ciaTimerB
-  attr_accessor :sid6581, :state
+  class C64Emulator
+    attr_reader :memory, :cpu, :ciaTimerA, :ciaTimerB
+    attr_accessor :sid6581, :state
 
-  def initialize(memory, sid6581)
-    @memory = memory
-    @cpu = Mos6510::Cpu.new(@memory, self)  # Passing both memory and a reference to C64Emulator
-    @ciaTimerA = CIATimer.new(self)
-    @ciaTimerB = CIATimer.new(self)
-    @sid6581 = sid6581
+    def initialize(memory, sid6581)
+      @memory = memory
+      @cpu = Mos6510::Cpu.new(@memory, self)  # Initialize CPU with memory and self-reference
+      @ciaTimerA = CIATimer.new(self)         # Initialize CIA Timer A
+      @ciaTimerB = CIATimer.new(self)         # Initialize CIA Timer B
+      @sid6581 = sid6581                      # SID chip instance
     end
 
     def load_sid_file(file_path)
-      sid_file = FileReader.read(file_path)
-      
-      # Ensure that @memory is an instance of the Memory class and is properly initialized
+      sid_file = FileReader.read(file_path)   # Read the SID file
+
+      # Ensure memory is properly initialized
       raise 'Memory not initialized' unless @memory.is_a?(Memory)
 
-      # Check if the start address is valid
+      # Validate the start address
       raise 'Invalid start address' unless @memory.valid_address?(sid_file.load_address)
 
-      load_program(sid_file.data, sid_file.load_address)
-      setup_sid_environment(sid_file)
+      load_program(sid_file.data, sid_file.load_address)  # Load the SID program
+      setup_sid_environment(sid_file)                     # Setup SID environment
     end
 
     def load_program(program_data, start_address)
+      # Validate the program data and start address
       raise 'Invalid program data' unless program_data.is_a?(Array)
       raise 'Invalid start address' unless valid_address?(start_address)
 
-      @cpu.load_program(program_data, start_address)
+      @cpu.load_program(program_data, start_address)  # Load program into CPU
     end
 
     def run
-      @cpu.reset
-      until @state.emulation_finished?
-        @cpu.step
-        emulate_cycle
+      @cpu.reset                                   # Reset CPU
+      until @state.emulation_finished?            # Run until emulation is finished
+        @cpu.step                                 # Execute CPU cycle
+        emulate_cycle                             # Execute additional emulation cycle
       end
     end
 
     def stop
-      @state.emulation_finished = true
+      @state.emulation_finished = true            # Flag to stop the emulation
     end
 
     def run_cycle    
-      @cpu.step
-      emulate_cycle
+      @cpu.step                                   # Execute a single CPU cycle
+      emulate_cycle                               # Emulate additional cycle activities
     end
 
     private
 
     def emulate_cycle
-      @state.update
-      @sid6581.generate_sound
-      @ciaTimerA.update
-      @ciaTimerB.update
-      # Add more logic as needed for the emulation cycle
+      @state.update                               # Update state
+      @sid6581.generate_sound                     # Generate SID sound
+      @ciaTimerA.update                           # Update CIA Timer A
+      @ciaTimerB.update                           # Update CIA Timer B
     end
 
     def setup_sid_environment(sid_file)
-      # Basic setup based on SID file properties
-      @cpu.pc = sid_file.init_address
+      @cpu.pc = sid_file.init_address             # Set CPU program counter
 
-      # Additional setup based on the version, flags, etc.
       if sid_file.version >= 2
-        handle_extended_sid_file(sid_file)
+        handle_extended_sid_file(sid_file)        # Handle extended SID features
       end
-
-      # Additional environment setup logic as required
     end
 
     def handle_extended_sid_file(sid_file)
-      # Logic to handle extended SID file features
-      # Example: setup for additional SID chips, handling speed settings, etc.
-      # Adjust based on the specifics of the sid_file object
+      # Implement extended SID features based on the SID file's specifications
     end
 
-    # Additional methods for SID operations, memory management, etc.
+    # Implement additional methods for SID operations and memory management
   end
 end
