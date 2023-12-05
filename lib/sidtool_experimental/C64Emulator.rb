1a67411cbdb995d6025a2f9252c420b65d1f093e
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-05 12:33:40 +0100

    Update C64Emulator.rb

--- lib/sidtool_experimental/C64Emulator.rb
+++ lib/sidtool_experimental/C64Emulator.rb
@@ -4,11 +4,11 @@ module SidtoolExperimental
 
     def initialize
       @memory = Memory.new
-      @cpu = Mos6510::Cpu.new(@memory, self) # Ensure two arguments are passed
+      @cpu = Mos6510::Cpu.new(@memory, self)
       @sid6581 = Sid6581.new(memory: @memory)
       @ciaTimerA = CIATimer.new(self)
       @ciaTimerB = CIATimer.new(self)
-      @state = State.new(@cpu, self, [@ciaTimerA, @ciaTimerB], @sid6581) 
+      @state = State.new(@cpu, self, [@ciaTimerA, @ciaTimerB], @sid6581) # Pass four arguments here
     end
 
     def load_sid_file(file_path)
