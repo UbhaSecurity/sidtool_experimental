55fa24dc1c8e5f0bb5461348c46af21ac202a9e2
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-05 10:04:31 +0100

    Update C64Emulator.rb

--- lib/sidtool_experimental/C64Emulator.rb
+++ lib/sidtool_experimental/C64Emulator.rb
@@ -11,10 +11,13 @@ module SidtoolExperimental
       @state = State.new(@cpu, self) # This should work now
     end
 
-   def load_sid_file(file_path)
+    def load_sid_file(file_path)
       sid_file = FileReader.read(file_path)
 
-      # Check if the start address is valid before proceeding
+      # Ensure that @memory is an instance of the Memory class and is properly initialized
+      raise 'Memory not initialized' unless @memory.is_a?(Memory)
+
+      # Check if the start address is valid
       raise 'Invalid start address' unless @memory.valid_address?(sid_file.load_address)
 
       load_program(sid_file.data, sid_file.load_address)
@@ -22,12 +25,7 @@ module SidtoolExperimental
     end
 
     def load_program(program_data, start_address)
-      raise 'Invalid program data' unless program_data.is_a?(Array)
-      raise 'Invalid start address' unless @memory.valid_address?(start_address)
-
-      program_data.each_with_index do |byte, offset|
-        @memory.write(start_address + offset, byte)
-      end
+      @cpu.load_program(program_data, start_address)
     end
 
     def run
@@ -49,16 +47,17 @@ module SidtoolExperimental
 
     private
 
-    def setup_sid_environment(sid_file)
-      # Setup the environment based on the SID file's properties
-      @cpu.pc = sid_file.init_address
-      # Additional setup as needed
-    end
-
     def emulate_cycle
       @state.update
       @sid6581.generate_sound
-      # Add more logic as needed
+      # Add more logic as needed for the emulation cycle
+    end
+
+    def setup_sid_environment(sid_file)
+      # Setup the environment based on the SID file's properties
+      # Example setup (adjust as necessary):
+      @cpu.pc = sid_file.init_address
+      # More setup logic here if needed
     end
 
     # Additional methods for SID operations, memory management, etc.
