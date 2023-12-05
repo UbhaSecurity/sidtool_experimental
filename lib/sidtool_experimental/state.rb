e2915c32b1b3d35c07e589f71dd8d3c1cc0b6f2c
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-05 01:29:45 +0100

    Update state.rb

--- lib/sidtool_experimental/state.rb
+++ lib/sidtool_experimental/state.rb
@@ -29,47 +29,32 @@ module SidtoolExperimental
       handle_nmi if nmi_pending?
     end
 
-def generate_interrupt(source)
-  case source
-  when :timer0
-    # Handle Timer 0 interrupt
-    # For example, set the IRQ flag in the processor status register (P)
-    @cpu.set_irq_flag
-  # Add more cases for other interrupt sources if needed
-  end
-end
+    def generate_interrupt(source)
+      case source
+      when :timer0
+        @cpu.set_irq_flag
+      # Add more cases for other interrupt sources if needed
+      end
+    end
 
-def perform_task_for_timer1
-  # Implement tasks specific to Timer 1 event
-  # For example, you can perform some actions here
-  # when Timer 1 reaches a certain condition
-  if timer1_condition_met?
-    # Perform the task
-    # Example: Display a message or update some state
-    puts "Timer 1 event occurred. Performing a task..."
-  end
-end
+    def perform_task_for_timer1
+      if timer1_condition_met?
+        puts "Timer 1 event occurred. Performing a task..."
+      end
+    end
 
-def timer1_condition_met?
-  # Implement the condition that defines when Timer 1 event occurs
-  # Return true if the condition is met, false otherwise
-  # Example: Check if Timer 1 counter reaches a specific value
-  @cia_timers[1].counter == specific_value
-end
+    def timer1_condition_met?
+      @cia_timers[1].counter == specific_value
+    end
 
-def handle_timer_0_expiration
-      # Example implementation for Timer 0 expiration event
-      # This might involve generating an IRQ interrupt
+    def handle_timer_0_expiration
       if timer_0_irq_enabled?
         @cpu.generate_interrupt(:irq)
-        # Additional logic specific to Timer 0, if any
       end
       # Other actions can be added here as needed
     end
 
     def handle_timer_1_expiration
-      # Example implementation for Timer 1 expiration event
-      # This could be handling a specific task or updating a state
       perform_specific_action_for_timer1
       # Any other logic that needs to be executed when Timer 1 expires
     end
@@ -80,81 +65,53 @@ def handle_timer_0_expiration
       @cia_timers.each(&:update)
     end
 
-def handle_timer_events
-  # Loop through CIA timers
-  @cia_timers.each_with_index do |timer, index|
-    # Check if a specific timer event condition is met
-    if timer.event_condition_met?
-      # Perform actions specific to the event
-      case index
-      when 0
-        # Timer 0 event handling
-        # Example: Generate an interrupt
-        generate_interrupt(:timer0)
-      when 1
-        # Timer 1 event handling
-        # Example: Perform a task
-        perform_task_for_timer1
+    def handle_timer_events
+      @cia_timers.each_with_index do |timer, index|
+        if timer.event_condition_met?
+          case index
+          when 0
+            generate_interrupt(:timer0)
+          when 1
+            perform_task_for_timer1
+          end
+        end
       end
     end
-  end
-end
 
-def update_sid
-  @sid6581.update(@current_frame)
-end
+    def update_sid
+      @sid6581.update(@current_frame)
+    end
 
     def increment_frame
       @current_frame += 1
     end
 
     def initialize_vectors
-      # Initialize IRQ, NMI, and BREAK vectors based on KERNAL ROM
       @irq_vector = 0xEA31
       @nmi_vector = 0xFE43
       @break_vector = 0xFE66
     end
 
-   # Helper method to check if IRQ is enabled for Timer 0
     def timer_0_irq_enabled?
       # Check the specific register or flag that controls IRQ for Timer 0
-      # Example: return true if IRQ enabled, else false
     end
 
-    # Example method for a specific action for Timer 1
     def perform_specific_action_for_timer1
       # Implement the specific action or task for Timer 1
-      # This could be anything from updating a state, triggering an event, etc.
     end
 
     def handle_irq
       if irq_pending?
-        # Save CPU state (registers, program counter, etc.)
         @cpu.save_state
-
-        # Jump to the IRQ vector address and execute the IRQ routine
         @cpu.jump_to_address(@irq_vector)
-
-        # The IRQ routine is responsible for acknowledging the IRQ
-        # and performing necessary actions
-
-        # Restore the CPU state after IRQ handling
         @cpu.restore_state
       end
     end
 
     def handle_nmi
       if nmi_pending?
-        # Save CPU state (registers, program counter, etc.)
         @cpu.save_state
-
-        # Jump to the NMI vector address and execute the NMI routine
         @cpu.jump_to_address(@nmi_vector)
-
-        # The NMI routine typically checks the cause of the NMI and handles it
-        # For example, it might handle the RUN/STOP + RESTORE keypress
-
-        # Restore the CPU state after NMI handling
         @cpu.restore_state
       end
     end
