9458b768ae5ab3d718f1c20ce578c4a667d7c6a2
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-05 17:34:17 +0100

    Update sidtool_experimental.rb

--- bin/sidtool_experimental.rb
+++ bin/sidtool_experimental.rb
@@ -40,9 +40,9 @@ module SidtoolExperimental
       exit(1)
     end
 
-memory = Memory.new
-sid6581 = Sid6581.new(memory: memory)
-c64_emulator = C64Emulator.new(memory, sid6581)
+  memory = Memory.new
+  sid6581 = Sid6581.new(memory: memory)
+  c64_emulator = C64Emulator.new(memory, sid6581)
   # Create State instance with the necessary components
   state = State.new(c64_emulator.cpu, c64_emulator, [c64_emulator.ciaTimerA, c64_emulator.ciaTimerB], sid6581)
 
