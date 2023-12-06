9eb6d623465c0f5a59179b36cbb2553449f52d32
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-06 19:30:06 +0100

    Update voice.rb

--- lib/sidtool_experimental/voice.rb
+++ lib/sidtool_experimental/voice.rb
@@ -180,12 +180,11 @@ module SidtoolExperimental
       @current_synth.release = release
     end
 
-    def finish_frame
-      # Process this voice's contribution to the audio for this frame
-      # Add the output to the SID's audio buffer
-      # Note: This is just a placeholder, actual implementation will depend on SID's audio synthesis logic
-      @sid6581.audio_buffer << generate_frame_output
-    end
+def finish_frame
+  output = generate_frame_output
+  puts "Generated output: #{output.inspect}"
+  @sid6581.audio_buffer << output
+end
 
    def oscillator_bit_19_high?
       # Implement the logic to check if oscillator bit 19 is high
