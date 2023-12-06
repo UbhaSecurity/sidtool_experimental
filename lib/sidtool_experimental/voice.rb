e86ca0e9714babb5cbac101d197a37b61f76f7c7
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-06 20:12:10 +0100

    Update voice.rb

--- lib/sidtool_experimental/voice.rb
+++ lib/sidtool_experimental/voice.rb
@@ -13,8 +13,9 @@ module SidtoolExperimental
     #
     # @param sid6581 [Sid6581] Reference to the SID chip instance.
     # @param voice_number [Integer] The number of the voice on the SID chip.
-    def initialize(sid6581, voice_number, state)
+    def initialize(sid6581,C64Emulator, voice_number, state)
       @sid6581 = sid6581
+      @C64Emulator = C64Emulator
       @synth = Synth.new(0, state)  # Pass the state here
       @voice_number = voice_number
       @frequency_low = @frequency_high = 0
