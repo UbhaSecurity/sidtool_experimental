7deb8e22923ce474ce026a7b396fba196f83ee9e
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-07 15:57:57 +0100

    Create generate_sid.rb

---
+++ bin/generate_sid.rb
@@ -0,0 +1,153 @@
+# Define the SID header v2
+header = "PSID"
+version = 2
+data_offset = 124  # Size of the header for version 2
+load_address = 0  # Default value for load address
+init_address = 0  # Default value for init address
+play_address = 0  # Default value for play address
+songs = 1
+start_song = 1
+speed = 0  # Default speed setting
+
+# New fields introduced in version 2
+flags = 0  # Initialize flags
+clock = 0  # Default clock (unknown)
+sid_model = 0  # Default SID model (unknown)
+start_page = 0  # Default start page for relocation
+page_length = 0  # Default page length for relocation
+reserved = 0  # Reserved field
+
+# Author, title, and released fields (32 bytes each)
+author = "Your Name".ljust(32, "\x00")
+title = "Your SID Tune Title".ljust(32, "\x00")
+released = "2023-12-31".ljust(32, "\x00")
+
+# Convert header fields to binary format
+header_data = [
+  header, version, data_offset, load_address, init_address, play_address, songs, start_song, speed,
+  flags, clock, sid_model, start_page, page_length, reserved,
+  author, title, released
+].pack("a4 S S S S S S S S S S S S C C S a32 a32 a32")
+
+# Define SID voice and register settings
+voice1 = "\x00\x00\x0f\x09\x00\x07\x00\x00"
+voice2 = "\x00\x00\x0f\x07\x00\x07\x00\x00"
+voice3 = "\x00\x00\x0f\x05\x00\x07\x00\x00"
+
+# Define a simple song structure with 5 patterns
+song_data = [
+  [0, 1, 2, 3, 4],   # Pattern order for voice 1
+  [0, 1, 2, 3, 4],   # Pattern order for voice 2
+  [0, 1, 2, 3, 4],   # Pattern order for voice 3
+]
+
+# Define note and effect data for each pattern
+patterns = []
+
+# Pattern 0 - Melody 1 (Main Track)
+patterns << [
+  # Voice 1
+  [
+    [60, 0x40, 0x0F],  # Note: 60, Attack: 64, Release: 15
+    [62, 0x40, 0x0F],
+    [64, 0x40, 0x0F],
+    [65, 0x40, 0x0F],
+  ],
+  # Voice 2 (Bass Track)
+  [
+    [36, 0x40, 0x0F],
+    [38, 0x40, 0x0F],
+    [40, 0x40, 0x0F],
+    [41, 0x40, 0x0F],
+  ],
+  # Voice 3
+  [
+    [72, 0x40, 0x0F],
+    [71, 0x40, 0x0F],
+    [69, 0x40, 0x0F],
+    [67, 0x40, 0x0F],
+  ],
+]
+
+# Define arpeggio data (e.g., ascending C major chord)
+arpeggio = [0, 4, 7]
+
+# Define slide data (e.g., slide up and down)
+slide_up = [0x0A, 0x08, 0x06, 0x04]
+slide_down = [0x05, 0x07, 0x09, 0x0B]
+
+# Define portamento effect (e.g., slide between notes)
+portamento = [0x00, 0x01, 0x02, 0x03]
+
+# Define volume control (e.g., decrease and increase volume)
+volume_down = [0x0F, 0x0D, 0x0B, 0x09, 0x07, 0x05, 0x03, 0x01]
+volume_up = [0x01, 0x03, 0x05, 0x07, 0x09, 0x0B, 0x0D, 0x0F]
+
+# Define vibrato effect (e.g., pitch modulation)
+vibrato = [0x00, 0x01, 0x02, 0x03]
+
+# Convert notes to frequencies (you may need to adjust these frequencies)
+freq_table = [
+  4186, 3951, 3729, 3520, 3322, 3136, 2960, 2794, 2637, 2489, 2349, 2217
+]
+
+# Define the SID data
+data = []
+
+# Generate the SID data for each pattern
+patterns.each_with_index do |pattern, pattern_index|
+  pattern.each_with_index do |voice_data, voice_index|
+    voice_data.each_with_index do |(note, attack, release), note_index|
+      freq = freq_table[note % 12]
+      note_data = [freq & 0xFF, (freq >> 8) & 0xFF, attack, release]
+
+      # Apply arpeggios, slides, portamento, volume control, vibrato
+      if voice_index == 0
+        # Apply arpeggio to voice 1
+        arpeggio_value = arpeggio[note_index % arpeggio.length]
+        note_data[1] |= (arpeggio_value & 0x0F) << 4
+      elsif voice_index == 1
+        # Apply slide up and down to bass track (voice 2)
+        slide_value = pattern_index.even? ? slide_up[note_index % slide_up.length] : slide_down[note_index % slide_down.length]
+        note_data[2] |= slide_value
+      elsif voice_index == 2
+        # Apply portamento effect to voice 3
+        portamento_value = portamento[note_index % portamento.length]
+        note_data[1] |= portamento_value
+      end
+
+      # Apply volume control (alternate volume up and down) to all voices
+      volume_value = pattern_index.even? ? volume_down[note_index % volume_down.length] : volume_up[note_index % volume_up.length]
+      note_data[2] |= (volume_value & 0x0F) << 4
+
+      # Apply vibrato effect to all voices
+      vibrato_value = vibrato[note_index % vibrato.length]
+      note_data[2] |= vibrato_value
+
+      data << note_data
+    end
+  end
+end
+
+# Write the SID file
+File.open("output.sid", "wb") do |file|
+  # Write header data
+  file.write(header_data)
+
+  # Write voice settings
+  file.write(voice1)
+  file.write(voice2)
+  file.write(voice3)
+
+  # Write song structure
+  song_data.each do |voice_order|
+    file.write(voice_order.pack("C*"))
+  end
+
+  # Write note and effect data
+  data.each do |note_data|
+    file.write(note_data.pack("C*"))
+  end
+end
+
+puts "SID file 'output.sid' generated successfully!"
