f89569d84ad46c5bfca1f8382fe6986c7d30fdd2
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-07 16:34:35 +0100

    Update generate_sid.rb

--- bin/generate_sid.rb
+++ bin/generate_sid.rb
@@ -5,19 +5,20 @@ data_offset = 118  # Size of the header for version 1
 load_address = 0  # Default value for load address
 init_address = 0  # Default value for init address
 play_address = 0  # Default value for play address
-songs = 4          # Increase the number of songs to 4
+songs = 4
 start_song = 1
-speed = 0          # Default speed setting
+speed = 0  # Default speed setting
 
 # Author, title, and released fields (32 bytes each)
 author = "Your Name".ljust(32, "\x00")
 title = "Your SID Tune Title".ljust(32, "\x00")
 released = "2023-12-31".ljust(32, "\x00")
 
+# Convert header fields to binary format
 header_data = [
   header, version, data_offset, load_address, init_address, play_address, songs, start_song, speed,
   author, title, released
-].pack("a4 S S S S S S S a32 a32 a32"
+].pack("a4 S S S S S S S a32 a32 a32")
 
 # Define SID voice and register settings
 voice1 = "\x00\x00\x0f\x09\x00\x07\x00\x00"
@@ -68,215 +69,32 @@ patterns << [
   ],
 ]
 
-# Pattern 1 - Melody 2
-patterns << [
-  # Voice 1
-  [
-    [64, 0x40, 0x0F],  # Note: 64, Attack: 64, Release: 15
-    [66, 0x40, 0x0F],
-    [68, 0x40, 0x0F],
-    [69, 0x40, 0x0F],
-  ],
-  # Voice 2 (Bass Track)
-  [
-    [36, 0x40, 0x0F],
-    [38, 0x40, 0x0F],
-    [40, 0x40, 0x0F],
-    [41, 0x40, 0x0F],
-  ],
-  # Voice 3
-  [
-    [76, 0x40, 0x0F],
-    [74, 0x40, 0x0F],
-    [72, 0x40, 0x0F],
-    [71, 0x40, 0x0F],
-  ],
-  # Bass Track
-  [
-    [24, 0x40, 0x0F],  # Adjust the notes and settings for your bass track
-    [26, 0x40, 0x0F],
-    [28, 0x40, 0x0F],
-    [29, 0x40, 0x0F],
-  ],
-]
+# Continue defining more patterns and melodies as needed...
 
-# Pattern 2 - Melody 3
-patterns << [
-  # Voice 1
-  [
-    [72, 0x40, 0x0F],  # Note: 72, Attack: 64, Release: 15
-    [71, 0x40, 0x0F],
-    [69, 0x40, 0x0F],
-    [67, 0x40, 0x0F],
-  ],
-  # Voice 2 (Bass Track)
-  [
-    [36, 0x40, 0x0F],
-    [38, 0x40, 0x0F],
-    [40, 0x40, 0x0F],
-    [41, 0x40, 0x0F],
-  ],
-  # Voice 3
-  [
-    [60, 0x40, 0x0F],
-    [62, 0x40, 0x0F],
-    [64, 0x40, 0x0F],
-    [65, 0x40, 0x0F],
-  ],
-  # Bass Track
-  [
-    [24, 0x40, 0x0F],  # Adjust the notes and settings for your bass track
-    [26, 0x40, 0x0F],
-    [28, 0x40, 0x0F],
-    [29, 0x40, 0x0F],
-  ],
-]
+# Write the SID file
+File.open("output.sid", "wb") do |file|
+  # Write header data
+  file.write(header_data)
 
-# Pattern 3 - Additional Bass Pattern
-patterns << [
-  # Voice 1
-  [
-    [55, 0x40, 0x0F],  # Note: 55, Attack: 64, Release: 15
-    [57, 0x40, 0x0F],
-    [59, 0x40, 0x0F],
-    [60, 0x40, 0x0F],
-  ],
-  # Voice 2 (Bass Track)
-  [
-    [33, 0x40, 0x0F],
-    [35, 0x40, 0x0F],
-    [36, 0x40, 0x0F],
-    [38, 0x40, 0x0F],
-  ],
-  # Voice 3
-  [
-    [67, 0x40, 0x0F],
-    [69, 0x40, 0x0F],
-    [71, 0x40, 0x0F],
-    [72, 0x40, 0x0F],
-  ],
-  # Bass Track
-  [
-    [24, 0x40, 0x0F],  # Adjust the notes and settings for your bass track
-    [26, 0x40, 0x0F],
-    [28, 0x40, 0x0F],
-    [29, 0x40, 0x0F],
-  ],
-]
+  # Write voice settings
+  file.write(voice1)
+  file.write(voice2)
+  file.write(voice3)
+  file.write(bass_track)
 
-# Pattern 4 - Repeating Bass Pattern
-patterns << [
-  # Voice 1
-  [
-    [60, 0x40, 0x0F],  # Note: 60, Attack: 64, Release: 15
-    [62, 0x40, 0x0F],
-    [64, 0x40, 0x0F],
-    [65, 0x40, 0x0F],
-  ],
-  # Voice 2 (Bass Track)
-  [
-    [36, 0x40, 0x0F],
-    [38, 0x40, 0x0F],
-    [40, 0x40, 0x0F],
-    [41, 0x40, 0x0F],
-  ],
-  # Voice 3
-  [
-    [72, 0x40, 0x0F],
-    [71, 0x40, 0x0F],
-    [69, 0x40, 0x0F],
-    [67, 0x40, 0x0F],
-  ],
-  # Bass Track
-  [
-    [24, 0x40, 0x0F],  # Adjust the notes and settings for your bass track
-    [26, 0x40, 0x0F],
-    [28, 0x40, 0x0F],
-    [29, 0x40, 0x0F],
-  ],
-]
+  # Write song structure
+  song_data.each do |voice_order|
+    file.write(voice_order.pack("C*"))
+  end
 
-# Pattern 5 - Ending Melody
-patterns << [
-  # Voice 1
-  [
-    [67, 0x40, 0x0F],  # Note: 67, Attack: 64, Release: 15
-    [69, 0x40, 0x0F],
-    [71, 0x40, 0x0F],
-    [72, 0x40, 0x0F],
-  ],
-  # Voice 2 (Bass Track)
-  [
-    [33, 0x40, 0x0F],
-    [35, 0x40, 0x0F],
-    [36, 0x40, 0x0F],
-    [38, 0x40, 0x0F],
-  ],
-  # Voice 3
-  [
-    [60, 0x40, 0x0F],
-    [62, 0x40, 0x0F],
-    [64, 0x40, 0x0F],
-    [65, 0x40, 0x0F],
-  ],
-  # Bass Track
-  [
-    [24, 0x40, 0x0F],  # Adjust the notes and settings for your bass track
-    [26, 0x40, 0x0F],
-    [28, 0x40, 0x0F],
-    [29, 0x40, 0x0F],
-  ],
-]
-
-# Generate pattern data
-data = []
-
-patterns.each do |pattern|
-  pattern.each_with_index do |voice_data, voice_index|
-    pattern_index = patterns.index(pattern)
-    
-    voice_data.each_with_index do |note_data, note_index|
-      # Apply pattern data
-      data << note_data[0]
-      data << note_data[1]
-
-      # Apply arpeggio to voice 1 (melody)
-      if voice_index == 0
-        arpeggio_values = [0, 1, 2]  # Adjust arpeggio values as needed
-        arpeggio_index = note_index % arpeggio_values.length
-        arpeggio_value = arpeggio_values[arpeggio_index]
-        data << arpeggio_value
-      end
-
-      # Apply slide and portamento to voice 2 (bass track)
-      if voice_index == 1
-        slide_value = 0x0F  # Adjust slide value as needed
-        data << slide_value
-        portamento_value = 0x00  # Adjust portamento value as needed
-        data << portamento_value
-      end
-
-      # Apply volume control to voice 3
-      if voice_index == 2
-        # Apply volume control here if needed
-        volume_value = 0x0F  # Adjust volume value as needed
-        data << volume_value
-      end
-
-      # Apply effects or additional settings
-      if voice_index == 3
-        # Apply volume control to bass track (voice 4)
-        volume_value = pattern_index.even? ? 0x0F : 0x07  # Adjust volume values as needed
-        data << volume_value
+  # Write note and effect data for each pattern
+  patterns.each do |pattern|
+    pattern.each do |voice_data|
+      voice_data.each do |note_data|
+        file.write(note_data.pack("C*"))
       end
     end
   end
 end
-
-# Combine header, voice settings, and data
-sid_data = header_data + voice1 + voice2 + voice3 + bass_track + song_data.flatten.pack("C*") + data.pack("C*")
-
-# Write the SID data to a file
-File.open("output.sid", "wb") { |file| file.write(sid_data) }
 
 puts "SID file 'output.sid' generated successfully!"
