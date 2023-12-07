36a06e990c5c46dab280d9ead2f6b4a7a2f7b36a
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-07 16:27:26 +0100

    Update generate_sid.rb

--- bin/generate_sid.rb
+++ bin/generate_sid.rb
@@ -1,7 +1,7 @@
-# Define the SID header v2
+# Define the SID header v1
 header = "PSID"
-version = 2
-data_offset = 124  # Size of the header for version 2
+version = 1
+data_offset = 118  # Size of the header for version 1
 load_address = 0  # Default value for load address
 init_address = 0  # Default value for init address
 play_address = 0  # Default value for play address
@@ -9,30 +9,16 @@ songs = 1
 start_song = 1
 speed = 0  # Default speed setting
 
-# New fields introduced in version 2
-flags = 0  # Initialize flags
-clock = 0  # Default clock (unknown)
-sid_model = 0  # Default SID model (unknown)
-start_page = 0  # Default start page for relocation
-page_length = 0  # Default page length for relocation
-reserved = 0  # Reserved field
-
 # Author, title, and released fields (32 bytes each)
 author = "Your Name".ljust(32, "\x00")
 title = "Your SID Tune Title".ljust(32, "\x00")
 released = "2023-12-31".ljust(32, "\x00")
 
+# Convert header fields to binary format
 header_data = [
-  header,
-  [version].pack("S"),
-  [data_offset, load_address, init_address, play_address, songs, start_song, speed].pack("S*"),
-  [flags, clock, sid_model, start_page, page_length, reserved].pack("C*"),
-  author,
-  title,
-  released
-].join
-
-
+  header, version, data_offset, load_address, init_address, play_address, songs, start_song, speed,
+  author, title, released
+].pack("a4 S S S S S S S S a32 a32 a32")
 
 # Define SID voice and register settings
 voice1 = "\x00\x00\x0f\x09\x00\x07\x00\x00"
@@ -138,16 +124,6 @@ end
 File.open("output.sid", "wb") do |file|
   # Write header data
   file.write(header_data)
-
-  # Write voice settings
-  file.write(voice1)
-  file.write(voice2)
-  file.write(voice3)
-
-  # Write song structure
-  song_data.each do |voice_order|
-    file.write(voice_order.pack("C*"))
-  end
 
   # Write note and effect data
   data.each do |note_data|
