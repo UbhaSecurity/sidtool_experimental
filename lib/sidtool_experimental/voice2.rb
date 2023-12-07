f480a794d00ce5a4d37e4bf48d2c526db25906b5
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-07 22:19:31 +0100

    Update voice2.rb

--- lib/sidtool_experimental/voice2.rb
+++ lib/sidtool_experimental/voice2.rb
@@ -264,27 +264,7 @@ end
 
     private
 
-  def generate_triangle_wave(phase)
-  # Calculate the oscillator value (upper 12 bits of the oscillator)
-  oscillator_value = (frequency_high << 4) | (frequency_low >> 4)
 
-  # Calculate the MSB of the oscillator (bit 23) based on the waveform selector
-  oscillator_msb = select_oscillator_msb(oscillator_value)
-
-  # Apply ring modulation effect if enabled (Control Register bit 2)
-  oscillator_msb = apply_ring_modulation(oscillator_msb, voice_number) if (@control_register & 0x04) != 0
-
-  # Calculate the result of the XOR logic for each of the 11 bits
-  triangle_bits = calculate_triangle_bits(oscillator_value, oscillator_msb)
-
-  # Combine the 11 bits to form the triangle waveform
-  triangle_waveform = combine_triangle_bits(triangle_bits)
-
-  # Scale the waveform to full amplitude (shift left and set LSB to 0)
-  scaled_waveform = triangle_waveform * 2.0 - 1.0
-
-  scaled_waveform
-end
 
 # Method to select the MSB of the oscillator based on the waveform selector
 def select_oscillator_msb(oscillator_value)
@@ -369,28 +349,6 @@ def calculate_modulating_oscillator_msb(modulating_oscillator_value)
   modulating_oscillator_msb
 end
       
-  def generate_sawtooth_wave(phase)
-    # Assuming phase is a value between 0 and 1 representing the current phase position
-
-    # Calculate the oscillator value (upper 12 bits) based on the current phase
-    oscillator_value = (phase * 0xFFF).to_i & 0xFFF
-
-    # Calculate the MSB of the oscillator
-    oscillator_msb = oscillator_value >> 11
-
-    # Check if ring modulation is enabled (Bit 2 of the Control Register)
-    if (@control_register & 0x04) == 0x04
-      # If ring modulation is enabled, obtain the MSB of the ring modulating voice's oscillator
-      modulating_oscillator_msb = get_ring_modulating_oscillator_msb(modulating_voice_number)
-      # XOR the MSBs to produce non-harmonic overtones
-      oscillator_msb ^= modulating_oscillator_msb
-    end
-
-    # Calculate the amplitude based on the MSB of the oscillator
-    amplitude = oscillator_msb.to_f / 0xFFF.to_f * 2.0 - 1.0
-
-    amplitude
-  end
 
   # Helper method to get the MSB of the ring modulating voice's oscillator
   def get_ring_modulating_oscillator_msb(modulating_voice_number)
@@ -399,27 +357,7 @@ end
     # Return the MSB value
   end
 
-def generate_pulse_wave(phase)
-  # Calculate the Pulse Width value from the PulseWidth register bits (12-bit value)
-  pulse_width = calculate_pulse_width
 
-  # Calculate the oscillator value (upper 12 bits)
-  oscillator_value = (frequency_high << 4) | (frequency_low >> 4)
-
-  # Calculate the result of the comparison between oscillator and Pulse Width
-  comparison_result = oscillator_value < pulse_width
-
-  # Check if the test bit is set (assuming it's stored in @test_bit)
-  if @test_bit
-    # If the test bit is set, invert the comparison result
-    comparison_result = !comparison_result
-  end
-
-  # Generate the pulse waveform based on the comparison result
-  amplitude = comparison_result ? 1.0 : -1.0
-
-  amplitude
-end
 
 # Method to calculate the Pulse Width value from PulseWidth register bits
 def calculate_pulse_width
@@ -433,31 +371,7 @@ def calculate_pulse_width
   pulse_width
 end
 
- def generate_noise_wave(phase)
-      # Clock the LFSR when bit 19 of the oscillator goes high
-      clock_lfsr if oscillator_bit_19_high?
-
-      # Output the selected bits (0, 2, 5, 9, 11, 14, 18, 20)
-      noise_output = (
-        ((@lfsr_state >> 0) & 0x01) << 0 |
-        ((@lfsr_state >> 2) & 0x01) << 2 |
-        ((@lfsr_state >> 5) & 0x01) << 5 |
-        ((@lfsr_state >> 9) & 0x01) << 9 |
-        ((@lfsr_state >> 11) & 0x01) << 11 |
-        ((@lfsr_state >> 14) & 0x01) << 14 |
-        ((@lfsr_state >> 18) & 0x01) << 18 |
-        ((@lfsr_state >> 20) & 0x01) << 20
-      )
-
-      # Invert the output to match the provided logic
-      inverted_output = invert_bits(noise_output)
-
-      # Convert to the range -1 to 1
-      normalized_output = (inverted_output * 2.0) - 1.0
-
-      normalized_output
-    end
-
+ 
 def handle_midi_note_change(new_midi_note)
   # Calculate the new frequency in Hertz based on the new MIDI note
   new_frequency_hz = midi_to_frequency(new_midi_note)
