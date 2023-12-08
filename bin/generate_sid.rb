51a982138e625334c5b7f31ab83c59d20c7a71b0
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-08 12:08:37 +0100

    Update generate_sid.rb

--- bin/generate_sid.rb
+++ bin/generate_sid.rb
@@ -1,11 +1,11 @@
-# Define the SID header v1
+# Define the SID header v1 for PSID file
 header = "PSID"
 version = 1
 data_offset = 118  # Size of the header for version 1
-load_address = 0  # Default value for load address
-init_address = 0  # Default value for init address
-play_address = 0  # Default value for play address
-songs = 4
+load_address = 0x1000  # Default load address (adjust as needed)
+init_address = 0x1000  # Default init address (adjust as needed)
+play_address = 0x1003  # Default play address (adjust as needed)
+songs = 1
 start_song = 1
 speed = 0  # Default speed setting
 
@@ -15,29 +15,21 @@ title = "Your SID Tune Title".ljust(32, "\x00")
 released = "2023-12-31".ljust(32, "\x00")
 
 # Convert header fields to binary format
-header_data = []
-header_data << header
-header_data << [version, data_offset, load_address, init_address, play_address, songs, start_song, speed]
-header_data.flatten!
-header_data << author
-header_data << title
-header_data << released
-
-# Convert header_data to binary
-header_data = header_data.pack("a4 S S S S S S S a32 a32 a32")
+header_data = [
+  header, version, data_offset, load_address, init_address, play_address, songs, start_song, speed,
+  author, title, released
+].pack("a4 S S S S S S S S a32 a32 a32")
 
 # Define SID voice and register settings
 voice1 = "\x00\x00\x0f\x09\x00\x07\x00\x00"
 voice2 = "\x00\x00\x0f\x07\x00\x07\x00\x00"
 voice3 = "\x00\x00\x0f\x05\x00\x07\x00\x00"
-bass_track = "\x00\x00\x0f\x04\x00\x07\x00\x00"  # New bass track
 
-# Define a simple song structure with 6 patterns for each voice
+# Define a simple song structure with 5 patterns
 song_data = [
-  [0, 1, 2, 3, 4, 5],   # Pattern order for voice 1
-  [0, 1, 2, 3, 4, 5],   # Pattern order for voice 2
-  [0, 1, 2, 3, 4, 5],   # Pattern order for voice 3
-  [0, 1, 2, 3, 4, 5],   # Pattern order for bass track
+  [0, 1, 2, 3, 4],   # Pattern order for voice 1
+  [0, 1, 2, 3, 4],   # Pattern order for voice 2
+  [0, 1, 2, 3, 4],   # Pattern order for voice 3
 ]
 
 # Define note and effect data for each pattern
@@ -66,42 +58,83 @@ patterns << [
     [69, 0x40, 0x0F],
     [67, 0x40, 0x0F],
   ],
-  # Bass Track
-  [
-    [24, 0x40, 0x0F],  # Adjust the notes and settings for your bass track
-    [26, 0x40, 0x0F],
-    [28, 0x40, 0x0F],
-    [29, 0x40, 0x0F],
-  ],
 ]
 
-# Continue defining more patterns and melodies as needed...
-# (Add more patterns and melodies using the same structure)
+# Define arpeggio data (e.g., ascending C major chord)
+arpeggio = [0, 4, 7]
 
-# Write the SID file
-File.open("output.sid", "wb") do |file|
-  # Write header data
-  file.write(header_data)
+# Define slide data (e.g., slide up and down)
+slide_up = [0x0A, 0x08, 0x06, 0x04]
+slide_down = [0x05, 0x07, 0x09, 0x0B]
 
-  # Write voice settings
-  file.write(voice1)
-  file.write(voice2)
-  file.write(voice3)
-  file.write(bass_track)
+# Define portamento effect (e.g., slide between notes)
+portamento = [0x00, 0x01, 0x02, 0x03]
 
-  # Write song structure
-  song_data.each do |voice_order|
-    file.write(voice_order.pack("C*"))
-  end
+# Define volume control (e.g., decrease and increase volume)
+volume_down = [0x0F, 0x0D, 0x0B, 0x09, 0x07, 0x05, 0x03, 0x01]
+volume_up = [0x01, 0x03, 0x05, 0x07, 0x09, 0x0B, 0x0D, 0x0F]
 
-  # Write note and effect data for each pattern
-  patterns.each do |pattern|
-    pattern.each do |voice_data|
-      voice_data.each do |note_data|
-        file.write(note_data.pack("C*"))
-      end
+# Define vibrato effect (e.g., pitch modulation)
+vibrato = [0x00, 0x01, 0x02, 0x03]
+
+# Convert notes to frequencies (you may need to adjust these frequencies)
+freq_table = [
+  4186, 3951, 3729, 3520, 3322, 3136, 2960, 2794, 2637, 2489, 2349, 2217,
+  2093, 1976, 1865, 1760, 1661, 1568, 1480, 1397, 1319, 1245, 1175, 1109,
+  1047, 987,  931,  880,  831,  784,  740,  699,  659,  622,  587,  554,
+  523,  494,  466,  440,  415,  392,  370,  349,  330,  311,  294,  277,
+  262,  247,  233,  220,  208,  196,  185,  175,  165,  156,  147,  139,
+  131,  123,  117,  110,  104,  98,   93,   87,   82,   78,   73,   69,
+  65,   62,   58,   55,   52,   49,   46,   44,   41,   39,   37,   35,
+]
+
+# Convert pattern data to binary format
+patterns_data = []
+
+patterns.each do |pattern|
+  pattern_data = []
+
+  pattern.each_with_index do |voice_data, voice_idx|
+    voice_pattern = []
+
+    voice_data.each do |note, attack, release|
+      # Convert note value to frequency (adjust as needed)
+      frequency = freq_table[note]
+
+      # Build the SID voice data
+      sid_data = [
+        [0x00, (frequency & 0xFF), ((frequency >> 8) & 0xFF)],
+        [attack, release, 0x00],
+      ].flatten
+
+      # Add arpeggio, slide, portamento, volume, and vibrato effects
+      effects = arpeggio + slide_up + slide_down + portamento + volume_down + volume_up + vibrato
+      sid_data += effects
+
+      voice_pattern << sid_data
     end
+
+    pattern_data << voice_pattern
   end
+
+  patterns_data << pattern_data
 end
 
-puts "SID file 'output.sid' generated successfully!"
+# Generate the SID pattern data (add more patterns as needed)
+pattern_data = patterns_data[0]
+
+# Convert pattern data to binary format
+pattern_data_binary = pattern_data.map do |voice_pattern|
+  voice_pattern.map do |sid_data|
+    sid_data.pack("C*")
+  end.join
+end.join
+
+# Generate the complete PSID file data
+psid_data = header_data + pattern_data_binary
+
+# Write the PSID data to an output file
+output_file = "output.sid"
+File.binwrite(output_file, psid_data)
+
+puts "PSID file '#{output_file}' generated successfully!"
