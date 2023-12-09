c1e2399441abe7a29eec17c3729d7d6b65f594d8
UbhaSecurity <ulf.daniel.bertilsson@gmail.com>
2023-12-09 03:34:23 +0100

    Update Sidgen.rb

--- bin/Sidgen.rb
+++ bin/Sidgen.rb
@@ -1,104 +1,86 @@
 MAGIC_NUMBER = "PSID"
 VERSION = 0x0002  # Hexadecimal representation of version 0002
+DEFAULT_PATTERN_LENGTH = 16
+DEFAULT_PLAY_ADDRESS = 0x1000
+PADDING_SIZE_BASE = 0x7C
+VOICE_CHANGE_POINT = 2
+STACCATO_DELAY = 0x00
+VOICE_2_SET = 0x0D
 
-# Function to convert a melody to SID data with envelope support, arpeggios, multiple voices, and song structure
-def convert_melody_to_sid_data(melody, pattern_length = 16)
+# Function to calculate SID note data
+def sid_note_data(note)
+  [
+    (note[:frequency] & 0xFF),
+    ((note[:frequency] >> 8) & 0xFF),
+    note[:waveform],
+    note[:attack_rate],
+    note[:decay_rate],
+    note[:sustain_level],
+    note[:release_rate]
+  ]
+end
+
+# Function to add arpeggio effect to SID data
+def add_arpeggio(sid_data, frequency, pattern_index)
+  if pattern_index % 3 == 0
+    sid_data.concat([
+      (frequency * 2 & 0xFF),
+      ((frequency * 2 >> 8) & 0xFF)
+    ])
+  end
+end
+
+# Function to convert a melody to SID data
+def convert_melody_to_sid_data(melody, pattern_length = DEFAULT_PATTERN_LENGTH)
   sid_data = []
   previous_note = nil
   voice = 0
-  pattern_index = 0
-  pattern_repeat = 0
 
-  while pattern_repeat < 2  # Repeat the pattern twice (for demonstration)
-    note = melody[pattern_index % melody.size]
+  melody.each_with_index do |note, index|
+    is_staccato = previous_note && previous_note[:frequency] == note[:frequency]
+    sid_data << STACCATO_DELAY if is_staccato
+    sid_data.concat(sid_note_data(note))
+    add_arpeggio(sid_data, note[:frequency], index) if voice == 1
 
-    frequency = note[:frequency]
-    waveform = note[:waveform]
-    attack_rate = note[:attack_rate]
-    decay_rate = note[:decay_rate]
-    sustain_level = note[:sustain_level]
-    release_rate = note[:release_rate]
-
-    # Check if the current note is the same as the previous note
-    is_staccato = previous_note && previous_note[:frequency] == frequency
-
-    # Add a delay for staccato effect
-    sid_data << 0x00 if is_staccato
-
-    # Create SID data for the note
-    sid_note_data = [
-      (frequency & 0xFF),            # Frequency (low byte)
-      ((frequency >> 8) & 0xFF),     # Frequency (high byte)
-      waveform,                      # Waveform
-      attack_rate,                   # Attack rate
-      decay_rate,                    # Decay rate
-      sustain_level,                 # Sustain level
-      release_rate                   # Release rate
-    ]
-
-    sid_data.concat(sid_note_data)
-
-    # Arpeggio effect (arpeggios every 3 notes)
-    if voice == 1 && pattern_index % 3 == 0
-      arpeggio_note_data = [
-        (frequency * 2 & 0xFF),      # Frequency (low byte)
-        ((frequency * 2 >> 8) & 0xFF)
-      ]
-      sid_data.concat(arpeggio_note_data)
-    end
-
-    # Switch to Voice 2 after a certain point
-    if melody.index(note) == melody.size / 2
+    if index == melody.size / VOICE_CHANGE_POINT
       voice = 1
-      sid_data << 0x0D  # Voice 2 set
+      sid_data << VOICE_2_SET
     end
 
-    # Update previous_note
     previous_note = note
-
-    # Move to the next pattern index
-    pattern_index += 1
-
-    # Check for pattern repetition
-    if pattern_index % pattern_length == 0
-      pattern_repeat += 1
-      # Skip the rest of the loop iteration if we've reached the end of pattern repetition
-      next if pattern_repeat >= 2
-    end
   end
 
-  return sid_data
+  sid_data
+end
+
+# Function to create a SID file header
+def create_sid_header(data_size, play_address)
+  header = "#{MAGIC_NUMBER}#{VERSION.to_s(16).rjust(4, '0')}"
+  version_checksum = header.each_byte.sum
+  padding_size = PADDING_SIZE_BASE - ((header.size + data_size) % PADDING_SIZE_BASE)
+
+  header + [data_size + 4].pack('V') + [play_address].pack('v') + [version_checksum].pack('v') + "\x00" * padding_size
 end
 
 # Function to create a SID file
-def create_sid_file(melody, filename, play_address = 0x1000)
-  # Calculate data size and padding size based on the length of the melody
-  data_size = melody.size * 7
-  padding_size = 0x7C - ((header.size + data_size) % 0x7C)
-
-  # Construct the SID file header with "505349440002"
-  header = "#{MAGIC_NUMBER}#{VERSION.to_s(16).rjust(4, '0')}"  # Version field is now 2 bytes
-
-  # Calculate the version field's checksum (sum of the ASCII values)
-  version_checksum = 0
-  header.each_byte { |byte| version_checksum += byte }
-
-  header += [data_size + 4].pack('V')  # Data size (plus 4 for play address and version)
-  header += [play_address].pack('v')  # Play address (2 bytes)
-  header += [version_checksum].pack('v')  # Version field checksum (2 bytes)
-  header += "\x00" * padding_size  # Padding with zero bytes
-
-  # Convert the melody to SID data
+def create_sid_file(melody, filename, play_address = DEFAULT_PLAY_ADDRESS)
   sid_data = convert_melody_to_sid_data(melody)
+  data_size = sid_data.size
+  header = create_sid_header(data_size, play_address)
 
-  # Write the header and SID data to the output file in binary mode
-  File.open(filename, 'wb') do |file|  # Use 'wb' for binary mode
+  File.open(filename, 'wb') do |file|
     file.write(header)
     file.write(sid_data.pack('C*'))
   end
 
   puts "SID file '#{filename}' created successfully."
 end
+
+# Example melody data (unchanged)
+melody_data = [
+  # ... melody data here ...
+]
+
 
 # Example melody data with enhanced features
 melody_data = [
@@ -155,5 +137,6 @@ melody_data = [
   { frequency: 392, waveform: 0, attack_rate: 15, decay_rate: 10, sustain_level: 5, release_rate: 10 }, # G
 ]
 
+
 # Output SID file with enhanced features
-create_sid_file(melody_data, "enhanced_melody.sid", 0x1000)  # Play address set to 0x1000
+create_sid_file(melody_data, "enhanced_melody.sid")
